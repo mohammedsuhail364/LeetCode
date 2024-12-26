f5dd6d9fa6be7d0378a54aacca96a2974b51c63b
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2024-12-26 22:57:14 +0530

    Time: 120 ms (76.4%), Space: 18 MB (63.39%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,5 +1,16 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        dp=[defaultdict(int) for _ in range(len(nums)+1)]
+        dp[0][0]=1
+        for i in range(len(nums)):
+            for cur_sum,count in dp[i].items():
+                dp[i+1][cur_sum+nums[i]]+=count
+                dp[i+1][cur_sum-nums[i]]+=count
+        return dp[len(nums)][target]
+
+
+
+
         dp={}
         def backtrack(i,cur_sum):
             if (i,cur_sum) in dp:
