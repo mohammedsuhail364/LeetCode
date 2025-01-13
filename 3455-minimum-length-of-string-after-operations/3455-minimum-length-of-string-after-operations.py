c83b3eaf5362da0cb7ec680665d0aa6faf368ce8
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2025-01-13 20:32:34 +0530

    Time: 139 ms (54.92%), Space: 18.9 MB (14.92%) - LeetHub

--- 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.py
+++ 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.py
@@ -1,30 +1,11 @@
 class Solution:
     def minimumLength(self, s: str) -> int:
-        s=list(s)
-        i=1
-        while i<len(s):
-            j=i-1
-            k=i+1
-            if s[i]=='':
-                i+=1
-                continue
-            while j>-1 and s[j]!=s[i]:
-                j-=1
-            if j==-1:
-                i+=1
-                continue
+        s=Counter(s)
+        total=0
+        for i,j in s.items():
+            if j%2==0:
+                total+=2
+            else:
+                total+=1
+        return total
             
-            while k<len(s) and s[k]!=s[i]:
-                k+=1
-            if k==len(s):
-                i+=1
-                continue
-            if s[j]==s[k]:
-                s[j]=''
-                s[k]=''
-            i+=1
-        c=0
-        for i in s:
-            if i != '':
-                c+=1
-        return c
