0dce9cb5882a4156801bddc8b07e450f7a4e6f28
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2025-01-14 09:13:27 +0530

    Time: 31 ms (30.73%), Space: 17.8 MB (19.53%) - LeetHub

--- 2766-find-the-prefix-common-array-of-two-arrays/2766-find-the-prefix-common-array-of-two-arrays.py
+++ 2766-find-the-prefix-common-array-of-two-arrays/2766-find-the-prefix-common-array-of-two-arrays.py
@@ -4,11 +4,10 @@ class Solution:
         res=[0]*len(A)
         for i in range(len(A)):
             new_a=A[:i+1]
-            new_b=B[:i+1]
+            new_b=set(B[:i+1])
             temp=0
             for x in new_a:
                 if x in new_b:
                     temp+=1
-            res[i]=temp
-                    
+            res[i]=temp              
         return res
