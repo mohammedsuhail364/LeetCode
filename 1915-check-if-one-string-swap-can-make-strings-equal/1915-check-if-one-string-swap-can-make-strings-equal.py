d63d52299061080dd3a7651decb19ec2e45e2b13
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2025-02-05 10:41:49 +0530

    Time: 0 ms (100%), Space: 17.6 MB (67.41%) - LeetHub

--- 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.py
+++ 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.py
@@ -1,13 +1,12 @@
 class Solution:
     def areAlmostEqual(self, s1: str, s2: str) -> bool:
-        if s1==s2:
-            return True
-        s1=list(s1)
-        s2=list(s2)
+        indexes=[]
         for i in range(len(s1)):
-            for j in range(i+1,len(s1)):
-                s1[i],s1[j]=s1[j],s1[i]
-                if s1==s2:
-                    return True
-                s1[i],s1[j]=s1[j],s1[i]
-        return False
+            if s1[i]!=s2[i]:
+                indexes.append(i)
+            if len(indexes)>2:
+                return False
+        if len(indexes)==2:
+            i,j=indexes
+            return s1[i]==s2[j] and s1[j]==s2[i]
+        return len(indexes)==0
