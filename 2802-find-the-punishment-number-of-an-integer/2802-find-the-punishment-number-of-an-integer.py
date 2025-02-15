bd3fa442aa6bf2280259a0e75bfcdef85fd35c4a
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2025-02-15 13:47:49 +0530

    Time: 1172 ms (36.61%), Space: 17.7 MB (68.75%) - LeetHub

--- 2802-find-the-punishment-number-of-an-integer/2802-find-the-punishment-number-of-an-integer.py
+++ 2802-find-the-punishment-number-of-an-integer/2802-find-the-punishment-number-of-an-integer.py
@@ -1,11 +1,15 @@
 class Solution:
     def punishmentNumber(self, n: int) -> int:
-        # def backtrack(k,i,index):
-        check={1,9,10,36,45,55,82,91,99,100,235,297,369,370,379,414,657,675,703,756,792,909,918,945,964,990,991,999,1000}
+        # check={1,9,10,36,45,55,82,91,99,100,235,297,369,370,379,414,657,675,703,756,792,909,918,945,964,990,991,999,1000}
+        def backtrack(index,cur,target,i_string):
+            if index==len(i_string) and cur==target:
+                return True
+            for j in range(index,len(i_string)):
+                if backtrack(j+1,cur+int(i_string[index:j+1]),target,i_string):
+                    return True
+            return False
         res=0
         for i in range(1,n+1):
-            
-            if i in check:
+            if backtrack(0,0,i,str(i*i)):
                 res+=(i*i)
-            
         return res
