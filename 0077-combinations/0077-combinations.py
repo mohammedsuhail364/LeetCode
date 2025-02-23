b627a1bbdac03d70b40a5ce26dbd493518197607
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2025-02-23 22:29:32 +0530

    Time: 141 ms (46.1%), Space: 58.9 MB (97.1%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,10 +1,14 @@
 class Solution:
-    def combine(self, n: int, k: int) -> List[List[int]]:
-        li=[]
-        li1=[]
-        for i in range(1,n+1):
-            li.append(i)
-        perm=combinations(li,k)
-        for i in perm:
-            li1.append(list(i))
-        return li1
+    def combine(self, n: int, k: int) :
+        self.res=[]
+        val=[i for i in range(1,n+1)]
+        def backtrack(res,start):
+            if len(res)==k:
+                self.res.append(res.copy())
+                return 
+            for i in range(start,len(val)):
+                res.append(val[i])
+                backtrack(res,i+1)
+                res.pop()
+        backtrack([],0)
+        return self.res
