fe3bbad0b0af5e4da3be5cc0d4a34fa3506dd5ac
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2025-03-04 10:17:02 +0530

    Time: 255 ms (5.58%), Space: 17.9 MB (18.33%) - LeetHub

--- 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.py
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.py
@@ -1,11 +1,11 @@
 class Solution:
     def checkPowersOfThree(self, n: int) -> bool:
-        def backtrack(power,n):
-            if n==0:
+        def backtrack(power,cur_sum):
+            if cur_sum==n:
                 return True
             if 3**power>n:
                 return False
-            add_power=backtrack(power+1,n-3**power)
-            skip_power=backtrack(power+1,n)
+            add_power=backtrack(power+1,cur_sum+3**power)
+            skip_power=backtrack(power+1,cur_sum)
             return add_power or skip_power
-        return backtrack(0,n)
+        return backtrack(0,0)
