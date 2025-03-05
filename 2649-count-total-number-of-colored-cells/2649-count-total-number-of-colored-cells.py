3760401ff3628748e9bdaa349623e33d356401ca
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2025-03-05 10:23:39 +0530

    Time: 119 ms (33.08%), Space: 17.6 MB (75.19%) - LeetHub

--- 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.py
+++ 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.py
@@ -1,19 +1,10 @@
 class Solution:
     def coloredCells(self, n: int) -> int:
         """
-        the pattern is n=1 -> 1
-        n -> 2    1+3+1
-        n -> 3    1+3+5+3+1
-        n -> 4    1+3+5+7+5+3+1
+        Another pattern is 1+4+8+12+....
+        
         """
-        cur_sum=0
-        odd_val=1
+        res=1
         for i in range(n):
-            cur_sum+=odd_val
-            odd_val+=2
-        odd_val-=4
-        for i in range(n-1):
-            cur_sum+=odd_val
-            odd_val-=2
-        return cur_sum
-
+            res+=(4*i)
+        return res
