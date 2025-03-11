84da019d76c6c6c6985ba4e5e7ba6249c3910367
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2025-03-11 10:30:39 +0530

    Time: 0 ms (100%), Space: 18 MB (8.45%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,16 +1,9 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        cache={}
-        def backtrack(cur_sum):
-            if cur_sum==n:
-                return 1
-            if cur_sum>n:
-                return 0
-            if cur_sum in cache:
-                return cache[cur_sum]
-            ways=0
-            for x in [1,2]:
-                ways+=backtrack(cur_sum+x)
-            cache[cur_sum]=ways
-            return ways
-        return backtrack(0)
+        i,j=0,1
+        for x in range(n):
+            third=i+j
+            i=j
+            j=third
+        return third
+        
