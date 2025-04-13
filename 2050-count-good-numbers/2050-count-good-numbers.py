50198326a70214fd980b167e98530dcb348f9b3c
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2025-04-13 14:21:42 +0530

    Time: 0 ms (100%), Space: 17.8 MB (44.07%) - LeetHub

--- 2050-count-good-numbers/2050-count-good-numbers.py
+++ 2050-count-good-numbers/2050-count-good-numbers.py
@@ -1,7 +1,17 @@
 class Solution:
     def countGoodNumbers(self, n: int) -> int:
-        MOD = 10**9 + 7
+        def find_power_val(x,n):
+            res=1
+            x=x%mod
+            while n>0:
+                if n%2:
+                    res=(res*x)%mod
+                x=(x*x)%mod
+                n=n//2
+            return res%mod
+
+        mod = 10**9 + 7
         even_position = (n+1)//2
         odd_position = n//2
-        return ((pow(5,even_position,MOD)*pow(4,odd_position,MOD))%MOD)
+        return ((find_power_val(5,even_position)*find_power_val(4,odd_position))%mod)
         
