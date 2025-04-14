9bbc1af0c3f3457d2afb7d479cab1615a98137f7
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2025-04-14 09:05:03 +0530

    Time: 184 ms (82.8%), Space: 17.8 MB (45.16%) - LeetHub

--- 1656-count-good-triplets/1656-count-good-triplets.py
+++ 1656-count-good-triplets/1656-count-good-triplets.py
@@ -3,7 +3,11 @@ class Solution:
         count=0
         for i in range(len(arr)):
             for j in range(i+1,len(arr)):
+                if abs(arr[i]-arr[j])>a:
+                    continue
                 for k in range(j+1,len(arr)):
-                    if abs(arr[i]-arr[j])<=a and abs(arr[j]-arr[k])<=b and abs(arr[i]-arr[k])<=c :
+                    if abs(arr[j]-arr[k])>b :
+                        continue
+                    if abs(arr[i]-arr[k])<=c :
                         count+=1
         return count
