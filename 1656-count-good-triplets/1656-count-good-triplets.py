de3e1f6eeba9cd834cf3a00d132559cd63660a2a
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2025-04-14 09:00:09 +0530

    Time: 303 ms (34.81%), Space: 17.8 MB (61.96%) - LeetHub

--- 1656-count-good-triplets/1656-count-good-triplets.py
+++ 1656-count-good-triplets/1656-count-good-triplets.py
@@ -1,8 +1,9 @@
 class Solution:
     def countGoodTriplets(self, arr: List[int], a: int, b: int, c: int) -> int:
         count=0
-        perm=combinations(arr,3)
-        for i in perm:
-            if a>=abs(i[0]-i[1]) and b>=abs(i[1]-i[2]) and c>=abs(i[0]-i[2]):
-                count+=1
+        for i in range(len(arr)):
+            for j in range(i+1,len(arr)):
+                for k in range(j+1,len(arr)):
+                    if abs(arr[i]-arr[j])<=a and abs(arr[j]-arr[k])<=b and abs(arr[i]-arr[k])<=c :
+                        count+=1
         return count
