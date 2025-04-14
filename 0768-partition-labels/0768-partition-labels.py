a902f26369e55e5c066ce0169ac46a814a4aac79
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2025-04-14 10:21:29 +0530

    Time: 2 ms (93.96%), Space: 17.8 MB (57.56%) - LeetHub

--- 0768-partition-labels/0768-partition-labels.py
+++ 0768-partition-labels/0768-partition-labels.py
@@ -1,11 +1,26 @@
+"""
+
+Problem Explanation:
+
+Given a string, divide it into partitions where:
+
+characters at each partition DOES NOT appear in any other partition
+then return the length of each one.
+Good Luck!
+
+Sol-
+Try to greedily choose the smallest partition that includes the first letter. 
+If you have something like "abaccbdeffed", then you might need to add b.
+You can use an map like "last['b'] = 5" to help you expand the width of your partition.
+
+"""
+
 class Solution:
     def partitionLabels(self, s: str):
         di={s1:i for i,s1 in enumerate(s)}   
-        # print(di)
         i=0
         res=[]
         while i<len(s):
-            
             last=di[s[i]]
             x=i
             while x<len(s) and x<=last:
@@ -13,5 +28,5 @@ class Solution:
                     last=di[s[x]]
                 x+=1
             res.append(last-i+1)
-            i=x
+            i=last+1
         return res
