3a3b904cef89db7710a672789cb865e48603f43e
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2025-05-20 23:05:43 +0530

    Time: 87 ms (49.85%), Space: 55 MB (52.93%) - LeetHub

---
+++ 3639-zero-array-transformation-i/3639-zero-array-transformation-i.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isZeroArray(self, nums: List[int], queries: List[List[int]]) -> bool:
+        n=len(nums)
+        diff=[0]*(n+1)
+        for i, j in queries:
+            diff[i]+=1
+            diff[j+1]-=1
+        pre_sum=0
+        pre_sum_arr=[0]*n
+        for i in range(n):
+            pre_sum_arr[i]=pre_sum+diff[i]
+            pre_sum=pre_sum_arr[i]
+        for i,j in zip(nums,pre_sum_arr):
+            if i>j:
+                return False
+        return True
