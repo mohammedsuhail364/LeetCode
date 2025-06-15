2bfd36057c5a3245c60cdbaccb4dffdf887a4bef
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2025-06-15 10:40:42 +0530

    Time: 3 ms (20.92%), Space: 18 MB (15.31%) - LeetHub

---
+++ 1529-max-difference-you-can-get-from-changing-an-integer/1529-max-difference-you-can-get-from-changing-an-integer.py
@@ -0,0 +1,35 @@
+class Solution:
+    def maxDiff(self, num: int) -> int:
+        s=list(str(num))
+        max_val=list(str(num))
+        min_val=list(str(num))
+        min_v=0
+        max_v=0
+        flag=False
+        for i in range(len(s)):
+            if s[i]!='1' and s[i]!='0':
+                min_v=s[i]
+                flag=True if i==0 else False
+                break
+        for i in range(len(s)):
+            if s[i]!='9':
+                max_v=s[i]
+                break
+        
+        for i in range(len(s)):
+            if s[i]==min_v and not flag:
+                min_val[i]='0'
+            elif s[i]==min_v and flag:
+                min_val[i]='1'
+            
+
+        for i in range(len(s)):
+            if s[i]==max_v:
+                max_val[i]='9'
+    
+        max_val=''.join(max_val)
+        min_val=''.join(min_val)
+        return int(max_val)-int(min_val)
+
+        
+        
