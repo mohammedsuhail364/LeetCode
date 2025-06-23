d2fcd3fcd59470d7c300944f368d370de4eaa5b7
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2025-06-23 14:59:35 +0530

    Time: 19 ms (8.47%), Space: 18.6 MB (16.72%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -1,25 +1,20 @@
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        par=[i for i in range(len(edges)+1)]
-        rank=[1]*(len(edges)+1)
-        
-        def find(n):
-            p=par[n]
-            while p!=par[p]:
-                par[p]=par[par[p]]
-                p=par[p]
-            return p
-        def union(n1,n2):
-            p1,p2=find(n1),find(n2)
-            if p1==p2:
-                return False
-            if rank[p1]>rank[p2]:
-                par[p2]=p1
-                rank[p1]+=rank[p2]
-            else:
-                par[p1]=p2
-                rank[p2]+=rank[p1]
-            return True
-        for n1,n2 in edges:
-            if not union(n1,n2):
-                return [n1,n2]
+        def dfs(node,parent,visit):
+            if node in visit:
+                return True
+            visit.add(node)
+            for nei in adj[node]:
+                if nei==parent:
+                    continue
+                if dfs(nei,node,visit):
+                    return True
+            return False
+        adj=defaultdict(list)
+        for src,dst in edges:
+            adj[src].append(dst)
+            adj[dst].append(src)
+            visit=set()
+            if dfs(src,-1,visit):
+                return [src,dst]
+        return []
