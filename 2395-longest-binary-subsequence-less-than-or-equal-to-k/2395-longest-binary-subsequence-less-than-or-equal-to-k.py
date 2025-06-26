d80b86a50a65d204157a44e71972944058e6b154
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2025-06-26 17:30:11 +0530

    Time: 7 ms (35.77%), Space: 17.9 MB (31.71%) - LeetHub

--- 2395-longest-binary-subsequence-less-than-or-equal-to-k/2395-longest-binary-subsequence-less-than-or-equal-to-k.py
+++ 2395-longest-binary-subsequence-less-than-or-equal-to-k/2395-longest-binary-subsequence-less-than-or-equal-to-k.py
@@ -2,14 +2,16 @@ class Solution:
     def longestSubsequence(self, s: str, k: int) -> int:
         if int(s,2)<=k:
             return len(s)
-        s=list(s)
+        val=int(s,2)
         res=len(s)
+        power=len(s)-1
         for i in range(len(s)):
             if s[i]=='1':
-                s[i]='0'
+                val-=2**power
                 res-=1
-            val=''.join(s)
-            if int(val,2)<=k:
+            
+            if val<=k:
                 return res
+            power-=1
         
             
