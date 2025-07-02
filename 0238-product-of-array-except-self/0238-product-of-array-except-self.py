78ec689ea73984b07ab9edfdaeada62dd612c229
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2025-07-02 21:48:15 +0530

    Time: 21 ms (70.45%), Space: 23.3 MB (79.7%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -3,10 +3,10 @@ class Solution:
         n=len(nums)
         res=[1]*n
         pre=1
+        suf=1
         for i in range(n):
             res[i]=pre
             pre*=nums[i]
-        suf=1
         for i in range(n-1,-1,-1):
             res[i]*=suf
             suf*=nums[i]
