ed7bbd039bf30c1d66ffd143557a78a72f794a66
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2025-07-09 12:16:00 +0530

    Time: 54 ms (33.75%), Space: 18.4 MB (77.28%) - LeetHub

--- 1049-minimum-domino-rotations-for-equal-row/1049-minimum-domino-rotations-for-equal-row.py
+++ 1049-minimum-domino-rotations-for-equal-row/1049-minimum-domino-rotations-for-equal-row.py
@@ -1,24 +1,21 @@
-from collections import defaultdict
-
 class Solution:
-    def minDominoRotations(self, tops, bottoms) -> int:
+    def minDominoRotations(self, tops: List[int], bottoms: List[int]) -> int:
         def helper(i):
             di=defaultdict(int)
-            
-            for x , y in zip(tops,bottoms):
+            for x,y in zip(tops,bottoms):
                 if x==i or y==i:
                     di[i]+=1
-            res=float('inf')
-            for i, j in di.items():
+            res=inf
+            for i,j in di.items():
                 if j==len(tops):
-                    if tops.count(i)>bottoms.count(i):
-                        res=min(len(tops)-tops.count(i),res)
-                    elif tops.count(i)<=bottoms.count(i):
-                        res=min(len(tops)-bottoms.count(i),res)
-            return res if res != float('inf') else -1
+                    top=tops.count(i)
+                    bottom=bottoms.count(i)
+                    if top>bottom:
+                        res=min(res,len(tops)-top)
+                    elif top<=bottom:
+                        res=min(res,len(tops)-bottom)
+            return -1 if res==inf else res
         res=helper(tops[0])
         if res!=-1:
             return res
         return helper(bottoms[0])
-        
-        
