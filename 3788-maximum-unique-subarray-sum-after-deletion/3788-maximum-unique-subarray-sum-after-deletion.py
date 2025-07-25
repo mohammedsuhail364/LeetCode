dc325a952cfc93fdd141c2c916dd31a3cf144734
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2025-07-25 11:01:38 +0530

    Time: 4 ms (10.77%), Space: 18 MB (11.24%) - LeetHub

--- 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.py
+++ 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.py
@@ -1,14 +1,10 @@
 class Solution:
     def maxSum(self, nums: List[int]) -> int:
+        if max(nums)<0:
+            return max(nums)
         c=Counter(nums)
         res=0
-        res1=-inf
         for i in c:
             if i>0:
                 res+=i
-        for i in c:
-            if i<0:
-                res1=max(i,res1)
-        if (res in c )or res:
-            return res
-        return res1
+        return res
