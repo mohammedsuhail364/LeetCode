0639af74d91f2b765c4ab0a943b1dac33edb2a83
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2025-08-23 15:16:30 +0530

    Time: 2989 ms (33.78%), Space: 48 MB (16.89%) - LeetHub

--- 3461-find-the-minimum-area-to-cover-all-ones-i/3461-find-the-minimum-area-to-cover-all-ones-i.py
+++ 3461-find-the-minimum-area-to-cover-all-ones-i/3461-find-the-minimum-area-to-cover-all-ones-i.py
@@ -2,11 +2,15 @@ class Solution:
     def minimumArea(self, grid: List[List[int]]) -> int:
         rows=len(grid)
         cols=len(grid[0])
-        row=set()
-        col=set()
+        min_row=rows
+        max_row=0
+        min_col=cols
+        max_col=0
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c]:
-                    row.add(r)
-                    col.add(c)
-        return (max(row) - min(row)+1) * (max(col)-min(col)+1)
+                    min_row=min(min_row,r)
+                    max_row=max(max_row,r)
+                    min_col=min(min_col,c)
+                    max_col=max(max_col,c)
+        return (max_row-min_row+1)*(max_col-min_col+1)
