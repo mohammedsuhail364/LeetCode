6aa602a986b203953d466ffbc5bbb1ba8424c3d3
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2025-09-13 14:27:57 +0530

    Time: 29 ms (0.05%), Space: 16.4 MB (100%) - LeetHub

--- 0394-decode-string/0394-decode-string.py
+++ 0394-decode-string/0394-decode-string.py
@@ -1,26 +1,17 @@
 class Solution:
     def decodeString(self, s: str) -> str:
         stack=[]
-        res=''
         for i in s:
-            if i !=']':
+            if i!=']':
                 stack.append(i)
             else:
-                string=''
-                number=''
-                while stack:
-                    val=stack.pop()
-                    if val=='[':
-                        break
-                    
-                    string = val + string
-                while stack:
-                    val=stack.pop()
-                    if not (val in '1234567890'):
-                        stack.append(val)
-                        break
-                    number+=val
-                number=int(number[::-1])
-                tmp=string*number
-                stack.append(tmp)
-        return ''.join(stack)    
+                subString=''
+                while stack and stack[-1]!='[':
+                    subString=stack.pop()+subString
+                stack.pop()
+                count=''
+                while stack and stack[-1].isdigit():
+                    count=stack.pop()+count
+                
+                stack.append(int(count)*subString)
+        return ''.join(stack)
