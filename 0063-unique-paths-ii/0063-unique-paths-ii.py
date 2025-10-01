702955c64e8c44914b08f37b474d0d66f78a00bf
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2025-10-01 11:04:36 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -2,22 +2,20 @@ class Solution:
     def uniquePathsWithObstacles(self, grid: List[List[int]]) -> int:
         if grid[0][0]==1 or grid[-1][-1]==1:
             return 0
+        self.res=0
         rows=len(grid)
         cols=len(grid[0])
-        dp={}
         def backtrack(r,c):
             if (r,c)==(rows-1,cols-1):
-                return 1
-            if (r,c) in dp:
-                return dp[(r,c)]
+                self.res+=1
+                return
+            temp=grid[r][c]
             grid[r][c]=-1
-            paths=0
             for nr,nc in [(r+1,c),(r,c+1)]:
                 if 0<=nr<rows and 0<=nc<cols and grid[nr][nc]==0:
-                    paths+=backtrack(nr,nc)
-            grid[r][c]=0
-            dp[(r,c)]=paths
-            return paths
-        
-        return backtrack(0,0)
+                    backtrack(nr,nc)
+            grid[r][c]=temp
+        backtrack(0,0)
+
+        return self.res
         
