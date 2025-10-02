636497ba9cda9f3faf39bdf87924acf1eaadb697
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2025-10-02 11:59:04 +0530

    Time: 11 ms (45.14%), Space: 19.8 MB (48.21%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.py
+++ 0740-delete-and-earn/0740-delete-and-earn.py
@@ -5,16 +5,11 @@ class Solution:
         di=Counter(nums)
         for i,j in di.items():
             new_nums[i]=(j*i)
-        
+        n=len(new_nums)
+        dp=[0]*(n+2)
         # similiar to house robber
-        memo={}
-        def dfs(i):
-            if i in memo:
-                return memo[i]
-            if i>=len(new_nums):
-                return 0
-            skip=dfs(i+1)
-            include=new_nums[i]+dfs(i+2)
-            memo[i] = max(skip,include)
-            return memo[i]
-        return dfs(0)
+        for i in range(n-1,-1,-1):
+            skip=dp[i+1]
+            include=new_nums[i]+dp[i+2]
+            dp[i]=max(skip,include)
+        return dp[0]
