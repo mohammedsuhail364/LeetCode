d3cca7adba38e4aeabe05698b93b7741aa1a7d9e
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2025-10-03 11:09:18 +0530

    Time: 135 ms (47.42%), Space: 19.8 MB (93.88%) - LeetHub

--- 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.py
+++ 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.py
@@ -1,24 +1,23 @@
 class Solution:
     def trapRainWater(self, heightMap: List[List[int]]) -> int:
-        # if you dont understand go and debug this 
         min_heap=[]
         rows=len(heightMap)
         cols=len(heightMap[0])
+        # add borders in heap
         for r in range(rows):
             for c in range(cols):
-                if r in [0,rows-1] or c in [0,cols-1]:
-                    heapq.heappush(min_heap,(heightMap[r][c],r,c))
+                if r in (0,rows-1) or c in (0,cols-1):
+                    heappush(min_heap,(heightMap[r][c],r,c))
                     heightMap[r][c]=-1
+        # start the multisource bfs
         res=0
-        max_hei=-1
+        max_height=-1
         while min_heap:
-            h,r,c=heapq.heappop(min_heap)
-            max_hei=max(max_hei,h)
-            res+=max_hei-h
-            neighbours=[(r,c+1),(r,c-1),(r+1,c),(r-1,c)]
-            for nr,nc in neighbours:
-                if (nr<0 or nc<0 or nr==rows or nc==cols or heightMap[nr][nc]==-1):
-                    continue
-                heapq.heappush(min_heap,(heightMap[nr][nc],nr,nc))
-                heightMap[nr][nc]=-1
+            h,r,c=heappop(min_heap)
+            max_height=max(max_height,h)
+            res+=(max_height-h)
+            for nr,nc in [(r+1,c),(r-1,c),(r,c+1),(r,c-1)]:
+                if 0<=nr<rows and 0<=nc<cols and heightMap[nr][nc]!=-1:
+                    heappush(min_heap,(heightMap[nr][nc],nr,nc))
+                    heightMap[nr][nc]=-1
         return res
