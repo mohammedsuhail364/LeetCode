472554816917cffd706b13d6bca424c90b60c1d5
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2025-10-04 20:28:43 +0530

    Time: 0 ms (100%), Space: 18 MB (8.77%) - LeetHub

--- 0376-wiggle-subsequence/0376-wiggle-subsequence.py
+++ 0376-wiggle-subsequence/0376-wiggle-subsequence.py
@@ -1,14 +1,10 @@
 class Solution:
     def wiggleMaxLength(self, nums: List[int]) -> int:
-        n=len(nums)
-        @lru_cache(None)
-        def dfs(i,prev,is_up):
-            if i>=n:
-                return 0
-            take=0
-            if prev==-1 or (nums[i]>nums[prev] and is_up) or (nums[prev]>nums[i] and not is_up):
-                take=1+dfs(i+1,i,not is_up)
-            skip=dfs(i+1,prev,is_up)
-            res=max(skip,take)
-            return res
-        return max(dfs(0,-1,True),dfs(0,-1,False))
+        inc=1
+        dec=1
+        for i in range(1,len(nums)):
+            if nums[i]>nums[i-1]:
+                inc=dec+1
+            elif nums[i]<nums[i-1]:
+                dec=inc+1 
+        return max(inc,dec)
