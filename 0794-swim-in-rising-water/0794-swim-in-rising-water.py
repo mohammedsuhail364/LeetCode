679c2d842a9b0d88a22c5df149aa48fdf548973d
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2025-10-06 10:05:09 +0530

    Time: 27 ms (66.76%), Space: 18.5 MB (53.12%) - LeetHub

--- 0794-swim-in-rising-water/0794-swim-in-rising-water.py
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.py
@@ -2,18 +2,15 @@ class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
         rows=len(grid)
         cols=len(grid[0])
-        min_heap=[(grid[0][0],0,0)] # time,row,col
-        visit=set()
-        visit.add((0,0))
-        res=0
+        min_heap=[]
+        heappush(min_heap,(grid[0][0],0,0))
+        visit=set([(0,0)])
         while min_heap:
-            time,r,c=heappop(min_heap)
-            if (r,c)==(rows-1,cols-1):
-                return max(res,grid[r][c])
-            res=max(res,time)
-            directions=[(r-1,c),(r+1,c),(r,c-1),(r,c+1)]
-            for nr,nc in directions:
+            h,r,c=heappop(min_heap)
+            if (r,c) == (rows-1,cols-1):
+                return h
+            for nr,nc in [(r+1,c),(r-1,c),(r,c+1),(r,c-1)]:
                 if 0<=nr<rows and 0<=nc<cols and (nr,nc) not in visit:
-                    heappush(min_heap,(grid[nr][nc],nr,nc))
+                    heappush(min_heap,(max(h,grid[nr][nc]),nr,nc))
                     visit.add((nr,nc))
         
