c6b6fcd1f0c154189c5dfa5b9a325a0d2e63db69
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2025-10-07 12:07:39 +0530

    Time: 12 ms (72.36%), Space: 20.7 MB (25.05%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -2,14 +2,12 @@ class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
         rows=len(grid)
         cols=len(grid[0])
-        memo={}
-        def dfs(r,c):
-            if (r,c) in memo:
-                return memo[r,c]
-            if r>=rows or c>=cols:
-                return float('inf')
-            if r==rows-1 and c==cols-1:
-                return grid[r][c]
-            memo[r,c] = grid[r][c] + min(dfs(r+1,c),dfs(r,c+1))
-            return memo[r,c]
-        return dfs(0,0)
+        dp=[[float('inf')]*(cols+1) for _ in range(rows+1)]
+        dp[rows-1][cols-1]=grid[rows-1][cols-1] # base case
+        for r in range(rows-1,-1,-1):
+            for c in range(cols-1,-1,-1):
+                if r==rows-1 and c==cols-1:
+                    continue
+                dp[r][c]=grid[r][c]+min(dp[r+1][c],dp[r][c+1])
+        return dp[0][0]
+        
