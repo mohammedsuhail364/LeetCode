7965f88528d5e34f3fd2ec39f87471eb22853463
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2025-10-08 12:03:31 +0530

    Time: 1103 ms (20.58%), Space: 46.1 MB (6.81%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,19 +1,19 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         memo={}
-        def dfs(cur_sum):
-            if cur_sum in memo:
-                return memo[cur_sum]
-            if cur_sum==amount:
+        def dfs(amount):
+            if amount in memo:
+                return memo[amount]
+            if amount==0:
                 return 0
-            if cur_sum>amount:
+            if amount<0:
                 return float('inf')
             res=float('inf')
             for c in coins:
-                res=min(res,1+dfs(cur_sum+c))
-            memo[cur_sum]=res
+                res=min(res,1+dfs(amount-c))
+            memo[amount]=res
             return res
-        ans=dfs(0)
+        ans=dfs(amount)
         if ans==float('inf'):
             return -1
         return ans
