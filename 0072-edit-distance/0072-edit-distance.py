8e9a6d01aeb3bf51a1c0ed065e518e2625cfc294
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2025-10-09 18:13:48 +0530

    Time: 67 ms (49.05%), Space: 21.2 MB (32.77%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -1,18 +1,20 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        memo={}
-        def dfs(i,j):
-            if (i,j) in memo:
-                return memo[i,j]
-            if i==len(word1): # word1 becomes empty
-                return len(word2)-j
-            if j==len(word2): # word2 becomes empty
-                return len(word1)-i
-            if word1[i]==word2[j]:
-                return dfs(i+1,j+1)
-            insert=dfs(i,j+1)
-            delete=dfs(i+1,j)
-            replace=dfs(i+1,j+1)
-            memo[i,j]=1+min(insert,delete,replace)
-            return memo[i,j]
-        return dfs(0,0)
+        # refer neetcode
+        n=len(word1)
+        m=len(word2)
+        dp=[[0]*(m+1) for _ in range(n+1)]
+        for i in range(n):
+            dp[i][m]=n-i
+        for j in range(m):
+            dp[n][j]=m-j
+        for i in range(n-1,-1,-1):
+            for j in range(m-1,-1,-1):
+                if word1[i]==word2[j]:
+                    dp[i][j]=dp[i+1][j+1]
+                else:
+                    insert=dp[i][j+1]
+                    delete=dp[i+1][j]
+                    replace=dp[i+1][j+1]
+                    dp[i][j]=1+min(insert,delete,replace)
+        return dp[0][0]
