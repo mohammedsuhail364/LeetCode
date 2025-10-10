9a2c0635e10db3c62cb65b922a79f80696903948
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2025-10-10 12:08:17 +0530

    Time: 58 ms (7.2%), Space: 17.8 MB (79.59%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.py
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -2,18 +2,18 @@ class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
         if len(s1)+len(s2)!=len(s3):
             return False 
-        memo={}
-        def dfs(i,j):
-            if (i,j) in memo:
-                return memo[i,j]
-            k=i+j # k is always sum of the i and j 
-            if i>=len(s1) and j>=len(s2):
-                return True
-            res=False
-            if i<len(s1) and s1[i]==s3[k]:
-                res = dfs(i+1,j)
-            if not res and j<len(s2) and s2[j]==s3[k]:
-                res = dfs(i,j+1)
-            memo[i,j]=res
-            return res
-        return dfs(0,0)
+        n=len(s1)
+        m=len(s2)
+        dp=[[False]*(m+1) for _ in range(n+1)] 
+        dp[n][m]=True # base case if we reach this we can use all the strings 
+        for i in range(n,-1,-1):
+            for j in range(m,-1,-1):
+                k=i+j
+                if i==m and j==n:
+                    continue # already set this
+                
+                if i<len(s1) and s1[i]==s3[k]:
+                    dp[i][j] =dp[i][j] or dp[i+1][j]
+                if not dp[i][j] and j<len(s2) and s2[j]==s3[k]:
+                    dp[i][j] =dp[i][j] or dp[i][j+1]
+        return dp[0][0]
