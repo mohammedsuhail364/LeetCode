d4513ab0a309d5a919deb0dadf3ab55ffd2e09bd
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2025-10-11 17:02:47 +0530

    Time: 474 ms (68.5%), Space: 39.6 MB (92.5%) - LeetHub

--- 3437-maximum-total-damage-with-spell-casting/3437-maximum-total-damage-with-spell-casting.py
+++ 3437-maximum-total-damage-with-spell-casting/3437-maximum-total-damage-with-spell-casting.py
@@ -1,21 +1,14 @@
 class Solution:
     def maximumTotalDamage(self, power) -> int:
+
         count=Counter(power)
         unique=sorted(count.keys())
-        memo={}
-        def dfs(i):
-            if i in memo:
-                return memo[i]
-            if i>=len(unique):
-                return 0
-            # this checks the every power as a starting point
-            res=dfs(i+1)
-            # include the valid ones
-            j=i+1
-            while j<len(unique) and unique[j]-unique[i]<=2:
-                j+=1
-            res=max(res,count[unique[i]]*unique[i]+dfs(j))
-            memo[i]=res
-            return res
-            
-        return dfs(0) 
+        dp=[0]*(len(unique)+1)
+        for i in range(len(unique)-1,-1,-1):
+            val=unique[i]
+            j=bisect.bisect_right(unique,val+2)
+            take=val*count[val]+dp[j]
+            skip=dp[i+1]
+            dp[i]=max(skip,take)
+        return dp[0]
+       
