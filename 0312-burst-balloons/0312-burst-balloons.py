f55e9cf76b9137338a057dabb61001693a944ac6
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2025-10-13 20:28:09 +0530

    Time: 2806 ms (70.97%), Space: 20.4 MB (69.69%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.py
+++ 0312-burst-balloons/0312-burst-balloons.py
@@ -1,19 +1,18 @@
 class Solution:
     def maxCoins(self, nums: List[int]) -> int:
-        # refer neetcode
+        # refer neetcode and take u forward
+
         nums=[1]+nums+[1]
         n=len(nums)
-        memo={}
-        def dfs(l,r):
-            if l>r:
-                return 0
-            if (l,r) in memo:
-                return memo[l,r]
-            best=0
-            for i in range(l,r+1):
-                coins=nums[l-1]*nums[i]*nums[r+1]
-                coins+=(dfs(l,i-1)+dfs(i+1,r))
-                best=max(best,coins)
-            memo[l,r]=best
-            return memo[l,r]
-        return dfs(1,n-2)
+        dp=[[0]*(n+2) for _ in range(n+2)]
+        for l in range(n,0,-1):
+            for r in range(1,n-1):
+                if l>r:continue
+                best=0
+                for i in range(l,r+1):
+                    coins=nums[l-1]*nums[i]*nums[r+1]
+                    coins+=(dp[l][i-1]+dp[i+1][r])
+                    best=max(best,coins)
+                dp[l][r]=best
+        return dp[1][n-2]
+        
