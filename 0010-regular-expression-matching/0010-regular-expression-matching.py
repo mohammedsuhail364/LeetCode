fb7324c40f9b56dbaf17bc943d7ab387ae10810f
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2025-10-14 21:32:47 +0530

    Time: 7 ms (69.25%), Space: 18.1 MB (27.06%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.py
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -1,18 +1,21 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         # refer neetcode
+        memo={}
         def dfs(i,j):
+            if (i,j) in memo:
+                return memo[i,j]
             if i>=len(s) and j>=len(p):
                 return True
             if j>=len(p):
                 return False
             match = (i<len(s) and (s[i]==p[j] or p[j]=="."))
             if j+1<len(p) and p[j+1]=="*":
-                ans=(dfs(i,j+2) or (match and dfs(i+1,j)))
-                return ans
+                memo[i,j]=(dfs(i,j+2) or (match and dfs(i+1,j)))
+                return memo[i,j]
             if match:
-                ans=dfs(i+1,j+1)
-                return ans
-            ans=False
+                memo[i,j]=dfs(i+1,j+1)
+                return memo[i,j]
+            memo[i,j]=False
             return False
         return dfs(0,0)
