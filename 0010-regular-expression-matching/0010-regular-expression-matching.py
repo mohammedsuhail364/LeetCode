cecbc409250030ae70512b3ae798a8c6e568e716
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2025-10-14 21:43:21 +0530

    Time: 11 ms (38.34%), Space: 17.7 MB (95.33%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.py
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -1,21 +1,16 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        # refer neetcode
-        memo={}
-        def dfs(i,j):
-            if (i,j) in memo:
-                return memo[i,j]
-            if i>=len(s) and j>=len(p):
-                return True
-            if j>=len(p):
-                return False
-            match = (i<len(s) and (s[i]==p[j] or p[j]=="."))
-            if j+1<len(p) and p[j+1]=="*":
-                memo[i,j]=(dfs(i,j+2) or (match and dfs(i+1,j)))
-                return memo[i,j]
-            if match:
-                memo[i,j]=dfs(i+1,j+1)
-                return memo[i,j]
-            memo[i,j]=False
-            return False
-        return dfs(0,0)
+        n=len(s)
+        m=len(p)
+        dp=[[False]*(m+1) for _ in range(n+1)]
+        dp[n][m]=True # base case
+        for i in range(n,-1,-1):
+            for j in range(m-1,-1,-1):
+                match = (i<len(s) and (s[i]==p[j] or p[j]=="."))
+                if j+1<len(p) and p[j+1]=="*":
+                    dp[i][j]=dp[i][j+2] 
+                    if match:
+                        dp[i][j]=dp[i+1][j] or dp[i][j]
+                elif match:
+                    dp[i][j]=dp[i+1][j+1]
+        return dp[0][0]
