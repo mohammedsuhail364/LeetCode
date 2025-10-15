7fa098c1ca2de3dbf3bff07af576b664edf698d8
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2025-10-15 19:32:55 +0530

    Time: 102 ms (94.87%), Space: 32.9 MB (75.69%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.py
+++ 0221-maximal-square/0221-maximal-square.py
@@ -1,19 +1,18 @@
 class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
+
         # refer neetcode
         ROWS,COLS=len(matrix),len(matrix[0])
-        memo={}
-        def dfs(r,c):
-            if r>=ROWS or c>=COLS:
-                return 0
-            if (r,c) in memo:
-                return memo[(r,c)]
-            memo[(r,c)]=0   
-            down=dfs(r+1,c)
-            right=dfs(r,c+1)
-            diagonal=dfs(r+1,c+1)
-            if matrix[r][c]=="1":
-                memo[(r,c)]=1+min(down,right,diagonal)
-            return memo[(r,c)]
-        dfs(0,0)
-        return max(memo.values())**2
+        dp=[[0]*(COLS+1) for _ in range(ROWS+1)]
+        for r in range(ROWS-1,-1,-1):
+            for c in range(COLS-1,-1,-1):
+                down=dp[r+1][c]
+                right=dp[r][c+1]
+                diagonal=dp[r+1][c+1]
+                if matrix[r][c]=="1":
+                    dp[r][c]=1+min(down,right,diagonal)
+        max_val=0
+        for r in dp:
+            max_val=max(max_val,max(r))
+        return max_val**2
+        
