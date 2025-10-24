a3c47e2b9c4ee27c86cc3d105082a3223d283ac4
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2025-10-24 17:19:47 +0530

    Time: 56 ms (5.75%), Space: 18.1 MB (14.93%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -1,15 +1,16 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        res=set()
-        def dfs(temp,i):
-            nonlocal res
-            if i==len(nums):
-                res.add(tuple(temp[:]))
-                return
-            temp.append(nums[i]) # include
-            dfs(temp,i+1)
-            temp.pop()
-            dfs(temp,i+1)
         nums.sort()
-        dfs([],0)
-        return [list(s) for s in res]
+        def dfs(i,path):
+            if i>=len(nums):
+                if path not in res:
+                    res.append(path)
+                return
+            # skip the current one
+            dfs(i+1,path)
+            # include the current one
+            for j in range(i,len(nums)):
+                dfs(j+1,path+[nums[j]])
+        res=[]
+        dfs(0,[])
+        return res
