6955f6a72b68289c4bd29f66750a405d1a001fd2
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2025-10-29 11:37:55 +0530

    Time: 518 ms (52.03%), Space: 30 MB (81.8%) - LeetHub

--- 3622-maximum-frequency-of-an-element-after-performing-operations-i/3622-maximum-frequency-of-an-element-after-performing-operations-i.py
+++ 3622-maximum-frequency-of-an-element-after-performing-operations-i/3622-maximum-frequency-of-an-element-after-performing-operations-i.py
@@ -1,17 +1,22 @@
 class Solution:
     def maxFrequency(self, nums: List[int], k: int, numOperations: int) -> int:
-        n=max(nums)
-        count=[0]*(n+1)
+        maximum=max(nums)+k+1
+        count=[0]*(maximum)
         for i in nums:
             count[i]+=1
-        presum=[0]
-        for i in range(n+1):
-            presum.append(presum[-1]+count[i])
+
+        for i in range(1,maximum):
+            count[i]=count[i-1]+count[i]
         res=0
-        for i in range(n+1):
-            left=presum[i]-presum[max(0,i-k)]
-            right=presum[min(n+1,i+k+1)]-presum[i+1]
-            cur=count[i]+min(numOperations,left+right)
-            res=max(res,cur)
+        for i in range(1,maximum):
+            left=max(0,i-k)
+            right=min(maximum-1,i+k)
+            left_most=max(0,left-1)
+            total=count[right]-count[left_most]
+            freq=count[i]-count[i-1]
+            res=max(res,freq+min(total-freq,numOperations))
         return res
 
+
+
+        
