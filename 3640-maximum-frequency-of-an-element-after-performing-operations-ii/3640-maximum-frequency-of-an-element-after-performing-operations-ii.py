571794367379ec61d091d29491ea5581f81a0eaa
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2025-10-29 15:20:42 +0530

    Time: 1014 ms (52.61%), Space: 63.2 MB (16.56%) - LeetHub

--- 3640-maximum-frequency-of-an-element-after-performing-operations-ii/3640-maximum-frequency-of-an-element-after-performing-operations-ii.py
+++ 3640-maximum-frequency-of-an-element-after-performing-operations-ii/3640-maximum-frequency-of-an-element-after-performing-operations-ii.py
@@ -1,44 +1,18 @@
-from collections import Counter, defaultdict
-
 class Solution:
     def maxFrequency(self, nums: List[int], k: int, numOperations: int) -> int:
-        # Frequency of each number in the array
-        freq = Counter(nums)
-
-        # Stores where the number of active intervals change (+start, -end)
-        interval_change = defaultdict(int)
-
-        # Mark interval starts and ends for each unique number
-        for num in freq:
-            start = num - k
-            end = num + k + 1  # +1 to make interval [num-k, num+k] inclusive
-            interval_change[start] += freq[num]
-            interval_change[end] -= freq[num]
-
-        # All possible points we need to consider
-        # (interval starts/ends + actual numbers)
-        all_points = sorted(interval_change.keys() | freq.keys())
-
-        # Sweeping variables
-        max_frequency = 0       # final result
-        active_intervals = 0    # how many numbers can affect this current point
-
-        # Sweep through all relevant points
-        for point in all_points:
-            # Update how many intervals are currently active
-            active_intervals += interval_change[point]
-
-            # How many numbers are already equal to this point
-            current_count = freq[point]
-
-            # We can increase the frequency by converting up to numOperations
-            # from other active intervals, but not more than available
-            possible_extra = min(active_intervals - current_count, numOperations)
-
-            # Total possible frequency for this value
-            possible_freq = current_count + possible_extra
-
-            # Update global maximum
-            max_frequency = max(max_frequency, possible_freq)
-
-        return max_frequency
+        freq=Counter(nums)
+        interval_change=defaultdict(int)
+        for num in nums:
+            start=num-k
+            end=num+k+1
+            interval_change[start]+=1
+            interval_change[end]-=1
+        points=sorted(interval_change.keys() | freq.keys())
+        active_intervals=0
+        res = 0 
+        for point in points:
+            active_intervals+=interval_change[point]
+            curr_count=freq[point]
+            extra=min(active_intervals-curr_count,numOperations)
+            res=max(res,extra+curr_count)
+        return res
