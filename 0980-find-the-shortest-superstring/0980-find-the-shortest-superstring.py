7bf46799e913cd6028e80da32449cd0bfca785cc
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2025-10-29 22:02:30 +0530

    Time: 3957 ms (5.44%), Space: 79.2 MB (6.12%) - LeetHub

--- 0980-find-the-shortest-superstring/0980-find-the-shortest-superstring.py
+++ 0980-find-the-shortest-superstring/0980-find-the-shortest-superstring.py
@@ -1,29 +1,28 @@
 class Solution:
     def shortestSuperstring(self, words: List[str]) -> str:
-        unused=(1<<len(words))-1
-        memo={}
+        n=len(words)
+        unused=(1<<n)-1
+        @lru_cache(None)
         def helper(start_word,unused):
             if unused==0:
                 return start_word
-            if (start_word,unused) in memo:
-                return memo[(start_word,unused)]
             shortest=None
-            for i in range(len(words)):
+            for i in range(n):
                 if unused & (1<<i):
-                    super_string=helper(words[i],unused ^ (1<<i))
+                    super_string=helper(words[i],unused^(1<<i))
                     overlap=overlap_append(start_word,super_string)
                     if shortest is None or len(overlap)<len(shortest):
                         shortest=overlap
-            memo[(start_word,unused)]=shortest
-            return memo[(start_word,unused)]
+            return shortest
         def overlap_append(a,b):
             max_overlap=""
             for i in range(1,min(len(a),len(b))+1):
-                suffix_of_A = a[-i:]
-                prefix_of_B = b[:i]
-                if suffix_of_A == prefix_of_B:
+                suffix_of_A=a[-i:]
+                prefix_of_B=b[:i]
+                if suffix_of_A==prefix_of_B:
                     remain_in_A=a[:-i]
                     remain_in_B=b[i:]
                     max_overlap=remain_in_A+suffix_of_A+remain_in_B
-            return max_overlap if max_overlap != "" else a+b
+            return max_overlap if max_overlap!="" else a+b 
+
         return helper("",unused)
