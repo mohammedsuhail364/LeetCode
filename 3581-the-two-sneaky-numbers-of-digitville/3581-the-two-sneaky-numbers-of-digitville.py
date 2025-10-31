1497422c2c3bddcb88192d14937b8d5d32ef074b
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2025-10-31 14:45:20 +0530

    Time: 0 ms (100%), Space: 17.6 MB (91.03%) - LeetHub

--- 3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.py
+++ 3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.py
@@ -1,4 +1,22 @@
 class Solution:
     def getSneakyNumbers(self, nums: List[int]) -> List[int]:
-        return [i for i,j in Counter(nums).items() if j==2]
-        
+        # refer neetcode
+        res=0
+        for n in nums:
+            res^=n
+        for i in range(len(nums)-2):
+            res^=i
+        res=res&-res
+        xor1,xor2=0,0
+        for n in nums:
+            if n&res:
+                xor1^=n
+            else:
+                xor2^=n
+        for i in range(len(nums)-2):
+            if i&res:
+                xor1^=i
+            else:
+                xor2^=i
+        return [xor1,xor2]
+
