a51e1f2223d815a504eb6e31d74a040e395539dc
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2025-11-05 21:00:56 +0530

    Time: 40 ms (8.48%), Space: 17.9 MB (38.82%) - LeetHub

--- 3610-find-x-sum-of-all-k-long-subarrays-i/3610-find-x-sum-of-all-k-long-subarrays-i.py
+++ 3610-find-x-sum-of-all-k-long-subarrays-i/3610-find-x-sum-of-all-k-long-subarrays-i.py
@@ -1,23 +1,65 @@
-from collections import Counter
-from typing import List
+from sortedcontainers import SortedList
 class Solution:
     def findXSum(self, nums: List[int], k: int, x: int) -> List[int]:
-        def getTopXvalues(arr):
-            freq=sorted(Counter(arr).items(),key=lambda x:(x[1],x[0]),reverse=True)
-            res=0
-            temp=0
-            for i,j in freq:
-                if temp==x:
-                    break
-                res+=(i*j)
-                temp+=1
-            return res
-        i=0 
-        j=k
+        # refer neetcode
+        top=SortedList()
+        remain=SortedList()
+        freq=defaultdict(int)
+        cur_sum=0
         res=[]
-        while j<=len(nums):
-            arr=nums[i:j]
-            res.append(getTopXvalues(arr))
-            i+=1
-            j+=1
+        def balance():
+            nonlocal cur_sum
+            if len(top)>x:
+                f,n=top.pop(0)
+                cur_sum-=(f*n)
+                remain.add((f,n))
+            if len(top)<x and remain:
+                f,n=remain.pop()
+                top.add((f,n))
+                cur_sum+=(f*n)
+            if top and remain and top[0]<remain[-1]:
+                f1,n1=top.pop(0)
+                f2,n2=remain.pop()
+                cur_sum-=(f1*n1)
+                top.add((f2,n2))
+                remain.add((f1,n1))
+                cur_sum+=(f2*n2)
+        def add(num):
+            nonlocal cur_sum
+            if num in freq:
+                prev=(freq[num],num)
+                if prev in top:
+                    top.remove(prev)
+                    cur_sum-=(freq[num]*num)
+                elif prev in remain:
+                    remain.remove(prev)
+            freq[num]+=1
+            top.add((freq[num],num))
+            cur_sum+=(freq[num]*num)
+            balance()
+        def remove(num):
+            nonlocal cur_sum
+            if num in freq:
+                prev=(freq[num],num)
+                if prev in top:
+                    top.remove(prev)
+                    cur_sum-=(freq[num]*num)
+                elif prev in remain:
+                    remain.remove(prev)
+            freq[num]-=1
+            if freq[num]==0:
+                del freq[num]
+            else:
+                top.add((freq[num],num))
+                cur_sum+=(freq[num]*num)
+            balance()
+
+        for i in range(k):
+            add(nums[i])
+        res.append(cur_sum)
+        for i in range(k,len(nums)):
+            remove(nums[i-k])
+            add(nums[i])
+            res.append(cur_sum)
         return res
+        
