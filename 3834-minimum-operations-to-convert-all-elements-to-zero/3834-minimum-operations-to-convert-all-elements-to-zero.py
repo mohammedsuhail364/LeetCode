ff1aff0adabe5285886d7c762bd41fb60544eb24
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2025-11-10 12:43:06 +0530

    Time: 196 ms (87.5%), Space: 29.7 MB (90.63%) - LeetHub

---
+++ 3834-minimum-operations-to-convert-all-elements-to-zero/3834-minimum-operations-to-convert-all-elements-to-zero.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        # refer neetcode
+        stack=[]
+        res=0
+        for n in nums:
+            while stack and stack[-1]>n:
+                stack.pop()
+            if n==0:
+                continue
+            if not stack or stack[-1]!=n:
+                res+=1
+                stack.append(n)
+        return res
