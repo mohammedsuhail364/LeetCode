395072dfb02f417d4ab95dc13192e1e50e3284a1
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2025-11-11 10:04:09 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0474-ones-and-zeroes/0474-ones-and-zeroes.py
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.py
@@ -1,20 +1,15 @@
 class Solution:
     def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
-        memo={}
-        def dfs(i,one_cnt,zero_cnt):
-            if (i,one_cnt,zero_cnt) in memo:
-                return memo[(i,one_cnt,zero_cnt)]
-            if i>=len(strs):
+        def dfs(i,zero,one):
+            if i>=len(strs) or zero>m or one>n:
                 return 0
             # skip the current one
-            skip=dfs(i+1,one_cnt,zero_cnt)
+            skip=dfs(i+1,zero,one)
             # include the current one
+            count_zero=(strs[i].count("0")+zero)
+            count_one=(strs[i].count("1")+one)
             include=0
-            zeros=strs[i].count('0')
-            ones=strs[i].count("1")
-            if zero_cnt+zeros<=m and one_cnt+ones<=n:
-                include = 1 + dfs(i+1,one_cnt+strs[i].count("1"),zero_cnt+strs[i].count('0'))
-            memo[(i,one_cnt,zero_cnt)] = max(skip,include)
-            return memo[(i,one_cnt,zero_cnt)]
+            if count_zero<=m and count_one<=n:
+                include=1+dfs(i+1,count_zero,count_one)
+            return max(skip,include)
         return dfs(0,0,0)
-            
