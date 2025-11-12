2709ac88c8bc09561715216126e3b460ccbaee64
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2025-11-12 14:37:38 +0530

    Time: 1007 ms (52.68%), Space: 63.3 MB (15.51%) - LeetHub

--- 3640-maximum-frequency-of-an-element-after-performing-operations-ii/3640-maximum-frequency-of-an-element-after-performing-operations-ii.py
+++ 3640-maximum-frequency-of-an-element-after-performing-operations-ii/3640-maximum-frequency-of-an-element-after-performing-operations-ii.py
@@ -2,17 +2,18 @@ class Solution:
     def maxFrequency(self, nums: List[int], k: int, numOperations: int) -> int:
         freq=Counter(nums)
         interval_change=defaultdict(int)
-        for num in nums:
-            start=num-k
-            end=num+k+1
+        for n in nums:
+            start=n-k
+            end=n+k+1
             interval_change[start]+=1
             interval_change[end]-=1
-        points=sorted(interval_change.keys() | freq.keys())
+        points=sorted(freq.keys()|interval_change.keys())
+        res=0
         active_intervals=0
-        res = 0 
-        for point in points:
-            active_intervals+=interval_change[point]
-            curr_count=freq[point]
+        for p in points:
+            active_intervals+=interval_change[p]
+            curr_count=freq[p]
+            # Between these points, active_intervals stays the same.
             extra=min(active_intervals-curr_count,numOperations)
             res=max(res,extra+curr_count)
         return res
