32c37079e23210ce7473695bd05649374790a22b
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2025-11-23 11:12:46 +0530

    Time: 299 ms (7.69%), Space: 142.3 MB (6.8%) - LeetHub

--- 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.py
+++ 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.py
@@ -1,23 +1,14 @@
 class Solution:
     def maxSumDivThree(self, nums: List[int]) -> int:
-        total=sum(nums)
-        if total%3==0:
-            return total
-        # when we mod the total it is either 1 or 2 or 0 we can handle the 0
-        mod1=[]
-        mod2=[]
-        for n in nums:
-            if n%3==1:
-                mod1.append(n)
-            elif n%3==2:
-                mod2.append(n)
-        mod1.sort()
-        mod2.sort()
-        if total%3==1:
-            option1=mod1[0] if len(mod1)>=1 else inf
-            option2=sum(mod2[:2]) if len(mod2)>=2 else inf
-            return total-min(option1,option2)
-        else: # total%3==2
-            option1=mod2[0] if len(mod2)>=1 else inf
-            option2=sum(mod1[:2]) if len(mod1)>=2 else inf
-            return total-min(option1,option2)
+        @lru_cache(None)
+        def dfs(i,remain):
+            if i>=len(nums):
+                if remain%3==0:
+                    return 0
+                return -inf
+            # skip the current one
+            skip=dfs(i+1,remain)
+            # include the current one
+            include=nums[i]+dfs(i+1,(remain+nums[i])%3)
+            return max(skip , include)
+        return dfs(0,0)
