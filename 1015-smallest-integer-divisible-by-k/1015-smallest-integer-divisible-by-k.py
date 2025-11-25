cdc7ad52519905c96271c10797ab282481ec3990
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2025-11-25 11:19:02 +0530

    Time: 3 ms (97.49%), Space: 17.7 MB (93.73%) - LeetHub

--- 1015-smallest-integer-divisible-by-k/1015-smallest-integer-divisible-by-k.py
+++ 1015-smallest-integer-divisible-by-k/1015-smallest-integer-divisible-by-k.py
@@ -4,14 +4,10 @@ class Solution:
         if k%2==0 or k%5==0:
             return -1
         cur=1
-        res=1
-        prev=set()
-        while cur%k!=0:
-            if cur in prev:
-                return -1 # cycle detection
-            prev.add(cur)
+        for res in range(1,k+1):
+            if cur%k==0:
+                return res
             cur=10*(cur%k)+1
-            res+=1
-        return res
+        return -1
         
         
