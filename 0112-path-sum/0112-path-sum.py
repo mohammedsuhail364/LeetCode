d2d440a81abb24f7fac142a8e9aa99b389e96a3c
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2025-11-26 21:14:56 +0530

    Time: 4 ms (8.97%), Space: 19 MB (23.15%) - LeetHub

--- 0112-path-sum/0112-path-sum.py
+++ 0112-path-sum/0112-path-sum.py
@@ -6,17 +6,11 @@
 #         self.right = right
 class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        
         if not root:
             return False
-        q=deque([(root,0)])
-        while q:
-            for i in range(len(q)):
-                node,val=q.popleft()
-                if not node.left and not node.right and val+node.val==targetSum:
-                    return True
-                if node.left:
-                    q.append((node.left,val+node.val))
-                if node.right:
-                    q.append((node.right,val+node.val))
-        return False
-        
+        if not root.right and not root.left:
+            return targetSum-root.val==0
+        left=self.hasPathSum(root.left,targetSum-root.val)
+        right=self.hasPathSum(root.right,targetSum-root.val)
+        return left or right
