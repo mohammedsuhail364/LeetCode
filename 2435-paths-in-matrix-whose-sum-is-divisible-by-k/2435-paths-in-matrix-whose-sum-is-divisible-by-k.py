0bde15fbc38dee3ea01073eaee76437ea56e1634
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2025-11-26 20:46:08 +0530

    Time: 2710 ms (25.56%), Space: 158.5 MB (29.15%) - LeetHub

--- 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.py
+++ 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.py
@@ -3,18 +3,20 @@ class Solution:
         rows=len(grid)
         cols=len(grid[0])
         MOD=10**9+7
-        memo={}
+        dp=[[[-1]*k for _ in range(cols)] for _ in range(rows)]
         def dfs(r,c,cur_sum):
-            if (r,c,cur_sum) in memo:
-                return memo[r,c,cur_sum]
             if r==rows-1 and c==cols-1:
                 if (cur_sum+grid[r][c])%k==0:
                     return 1
                 return 0
             if r>=rows or r<0 or c>=cols or c<0:
                 return 0
-            
-            count=(dfs(r+1,c,(cur_sum+grid[r][c])%k)+dfs(r,c+1,(cur_sum+grid[r][c])%k))
-            memo[r,c,cur_sum]= count%MOD
-            return memo[r,c,cur_sum]
-        return dfs(0,0,0)%MOD
+            if dp[r][c][cur_sum] != -1:
+                return dp[r][c][cur_sum]
+            count=(
+                dfs(r+1,c,(cur_sum+grid[r][c])%k)+
+                dfs(r,c+1,(cur_sum+grid[r][c])%k)
+                )
+            dp[r][c][cur_sum]= count%MOD
+            return dp[r][c][cur_sum]
+        return dfs(0,0,0)
