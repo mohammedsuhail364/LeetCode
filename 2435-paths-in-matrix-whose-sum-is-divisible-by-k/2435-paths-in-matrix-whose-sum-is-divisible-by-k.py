0304a3d0a8c1412aa120e53a863319bb2e1519ba
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2025-11-26 20:52:52 +0530

    Time: 1605 ms (76.68%), Space: 99 MB (38.57%) - LeetHub

--- 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.py
+++ 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.py
@@ -1,22 +1,20 @@
 class Solution:
     def numberOfPaths(self, grid: List[List[int]], k: int) -> int:
+        # refer neetcode
         rows=len(grid)
         cols=len(grid[0])
         MOD=10**9+7
-        dp=[[[-1]*k for _ in range(cols)] for _ in range(rows)]
-        def dfs(r,c,cur_sum):
-            if r==rows-1 and c==cols-1:
-                if (cur_sum+grid[r][c])%k==0:
-                    return 1
-                return 0
-            if r>=rows or r<0 or c>=cols or c<0:
-                return 0
-            if dp[r][c][cur_sum] != -1:
-                return dp[r][c][cur_sum]
-            count=(
-                dfs(r+1,c,(cur_sum+grid[r][c])%k)+
-                dfs(r,c+1,(cur_sum+grid[r][c])%k)
-                )
-            dp[r][c][cur_sum]= count%MOD
-            return dp[r][c][cur_sum]
-        return dfs(0,0,0)
+        dp=[[[0]*k for _ in range(cols+1)] for _ in range(rows+1)]
+        target_remain = (k-(grid[rows-1][cols-1]%k))%k
+        dp[rows-1][cols-1][target_remain]=1
+        for r in range(rows-1,-1,-1):
+            for c in range(cols-1,-1,-1):
+                if r==rows-1 and c==cols-1:
+                    continue
+                for remain in range(k):
+                    new_remain=(remain+grid[r][c])%k
+                    dp[r][c][remain]=(
+                        dp[r+1][c][new_remain]%MOD+
+                        dp[r][c+1][new_remain]%MOD
+                    )%MOD
+        return dp[0][0][0]
