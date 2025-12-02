375ca2b1856351d5bd9c150afa6de10b06320103
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2025-12-02 12:04:47 +0530

    Time: 157 ms (19.15%), Space: 32.4 MB (14.45%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
@@ -6,15 +6,16 @@
 #         self.right = right
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
-        q=deque([(root,float('-inf'))])
         res=0
-        while q:
-            for x in range(len(q)):
-                node,max_val=q.popleft()
-                if node.val>=max_val:
-                    res+=1
-                if node.left:
-                    q.append((node.left,max(max_val,node.val)))
-                if node.right:
-                    q.append((node.right,max(max_val,node.val)))
+        def dfs(node,cur_max):
+            nonlocal res
+            if not node:
+                return
+            if cur_max<=node.val:
+                res+=1
+            cur_max=max(cur_max,node.val)
+            dfs(node.left,cur_max)
+            dfs(node.right,cur_max)
+        dfs(root,root.val)
         return res
+            
