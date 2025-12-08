671bd81539151aaee30f3b638af51b09812bced0
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2025-12-08 20:37:58 +0530

    Time: 136 ms (28.41%), Space: 18.3 MB (33.92%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,22 +1,25 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        s_count=Counter()
-        t_count=Counter(t)
-        l=0
-        res=''
+        t_freq=Counter(t)
+        s_freq=defaultdict(int)
         have=0
-        min_len=float('inf')
-        need=len(t_count)
+        need=len(t_freq)
+        l=0
+        res=""
+        min_len=inf
         for r in range(len(s)):
-            s_count[s[r]]+=1
-            if s_count[s[r]]==t_count[s[r]]:
+            s_freq[s[r]]+=1
+            if t_freq[s[r]]== s_freq[s[r]]:
                 have+=1
             while need==have:
                 if (r-l+1)<min_len:
+                    min_len=(r-l+1)
                     res=s[l:r+1]
-                    min_len=r-l+1
-                s_count[s[l]]-=1
-                if s[l] in t_count and s_count[s[l]]<t_count[s[l]]:
+                s_freq[s[l]]-=1
+                if s[l] in t_freq and t_freq[s[l]]>s_freq[s[l]]:
                     have-=1
                 l+=1
+            
         return res
+            
+            
