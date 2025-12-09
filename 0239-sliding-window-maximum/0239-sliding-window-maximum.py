83cd69c28264886c22c6533bd0e65cbade71d31f
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2025-12-09 12:02:03 +0530

    Time: 152 ms (80.31%), Space: 31.5 MB (51.05%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -1,15 +1,17 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        l=r=0
-        output=[]
-        q=deque()
+        l=0
+        q=deque() # stores index (easy to track the window)
+        res=[]
         for r in range(len(nums)):
+            # maintain the monotonic stack 
             while q and nums[q[-1]]<nums[r]:
                 q.pop()
             q.append(r)
+            # maintain the window
             if l>q[0]:
                 q.popleft()
-            if (r+1)>=k:
-                output.append(nums[q[0]])
+            if (r+1)>=k: # found the window
+                res.append(nums[q[0]])
                 l+=1
-        return output
+        return res
