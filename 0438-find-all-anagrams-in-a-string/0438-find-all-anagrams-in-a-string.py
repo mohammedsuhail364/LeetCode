26aa6ddcd4b11897b8b24b790c1a088589e4e7c9
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2025-12-12 21:15:34 +0530

    Time: 59 ms (48.98%), Space: 18.2 MB (80.41%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -1,21 +1,16 @@
 class Solution:
-    def findAnagrams(self, s: str, p: str):
-        def convert_to_index(i):
-            return ord(i)-ord('a')
-        p_count=[0]*26
-        s_count=[0]*26
-        for i in p:
-            index=convert_to_index(i)
-            p_count[index]+=1
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        p_freq=Counter(p)
+        s_freq=defaultdict(int)
         l=0
         res=[]
         for r in range(len(s)):
-            index=convert_to_index(s[r])
-            s_count[index]+=1
+            s_freq[s[r]]+=1
             if (r-l+1)>len(p):
-                index=convert_to_index(s[l])
-                s_count[index]-=1
+                s_freq[s[l]]-=1
+                if s_freq[s[l]]==0:
+                    del s_freq[s[l]]
                 l+=1
-            if s_count==p_count:
+            if p_freq==s_freq:
                 res.append(l)
         return res
