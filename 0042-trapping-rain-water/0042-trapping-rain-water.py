b54b537dba6ce1e6da6d7cd16d1cf5d9c038f826
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2025-12-15 15:32:26 +0530

    Time: 31 ms (19.53%), Space: 19.1 MB (92.94%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,24 +1,18 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        maxLeft=0
-        maxLeftArr=[]
-        ans=0
-        for i in range(len(height)):
-            maxLeftArr.append(maxLeft)
-            if height[i]>maxLeft:
-                maxLeft=height[i]
-        print(maxLeftArr)  
-        maxRight=0
-        maxRightArr=[]
-        for i in range(len(height)-1,-1,-1):
-            maxRightArr.append(maxRight)
-            if height[i]>maxRight:
-                maxRight=height[i]
-        print(maxRightArr) 
-        maxRightArr=maxRightArr[::-1]
-        for i in range(len(height)):
-            val =min(maxLeftArr[i],maxRightArr[i])-height[i]
-            if val>0:
-                ans+=val
-        return ans
-            
+        n=len(height)
+        left_max=[0]*n
+        right_max=[0]*n
+        # find the left max
+        left_max[0]=height[0]
+        for i in range(1,n):
+            left_max[i]=max(left_max[i-1],height[i])
+        # fin the right max
+        right_max[n-1]=height[n-1]
+        for i in range(n-2,-1,-1):
+            right_max[i]=max(right_max[i+1],height[i])
+        # find the water stored in the between space
+        water=0
+        for i in range(n):
+            water+=max(0,min(left_max[i],right_max[i])-height[i])
+        return water
