d844fcd7811c076ce6e5e2cdcda41f9256bb0b56
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2025-12-25 11:40:40 +0530

    Time: 0 ms (100%), Space: 17.6 MB (96.85%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,12 +1,24 @@
 class Solution:
     def searchMatrix(self, matrix, target: int) -> bool:
+        def is_same(mid):
+            nums=matrix[mid]
+            l=0
+            r=len(nums)-1
+            while l<=r:
+                m=(l+r)//2
+                if nums[m]==target:
+                    return True
+                elif nums[m]>target:
+                    r=m-1
+                else:
+                    l=m+1
+            return False
         l=0
         r=len(matrix)-1
         while l<=r:
             m=(l+r)//2
-            for x in matrix[m]:
-                if x==target:
-                    return True
+            if is_same(m):
+                return True
             if matrix[m][0]>target:
                 r=m-1
             else:
