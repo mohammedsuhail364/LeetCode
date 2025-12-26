3324ddfe75ec444cb68a21de025974e88022d30e
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2025-12-26 13:47:31 +0530

    Time: 2 ms (73.15%), Space: 17.3 MB (95.57%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.py
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.py
@@ -1,19 +1,28 @@
 class Solution:
     def splitArray(self, nums: List[int], k: int) -> int:
-        dp={}
-        def dfs(i,m):
-            if m==1:
-                return sum(nums[i:])
-            if (i,m) in dp:
-                return dp[(i,m)]
+        def is_valid(max_allowed):
+            sub_array=1
             cur_sum=0
-            res=inf
-            for j in range(i,len(nums)-m+1):
-                cur_sum+=nums[j]
-                max_sum=max(cur_sum,dfs(j+1,m-1))
-                res=min(res,max_sum)
-                if cur_sum>res:
-                    break
-            dp[(i,m)]=res
-            return dp[(i,m)]
-        return dfs(0,k)
+            for n in nums:
+                if cur_sum+n<=max_allowed:
+                    cur_sum+=n
+                else:
+                    cur_sum=n
+                    sub_array+=1
+                    if sub_array>k:
+                        return False
+            return True
+        # this is similiar to koko eating bananas and mainly same to 1011. Capacity To Ship Packages Within D Days
+        # first we can set the range (max(nums),sum(nums)) why ? because if we start with 0 or 1 it is useless
+        l=max(nums)
+        r=sum(nums)
+        res=0
+        while l<=r:
+            m=(l+r)//2
+            # in this context m refers maximum value of split array we can check this is minimum of maximum
+            if is_valid(m):
+                res=m # find one of the value want to minimize this
+                r=m-1
+            else:
+                l=m+1
+        return res
