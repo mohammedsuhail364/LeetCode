7b162db99b7ff55c33dfed8c0fea6c540c73333b
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2026-01-13 21:39:06 +0530

    Time: 19 ms (38.37%), Space: 21 MB (9.54%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,18 +1,17 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
+        # find the left max
         n=len(height)
         left_max=[0]*n
         right_max=[0]*n
-        # find the left max
         left_max[0]=height[0]
-        for i in range(1,n):
+        for i in range(1,len(height)):
             left_max[i]=max(left_max[i-1],height[i])
-        # fin the right max
         right_max[n-1]=height[n-1]
         for i in range(n-2,-1,-1):
             right_max[i]=max(right_max[i+1],height[i])
-        # find the water stored in the between space
         water=0
         for i in range(n):
-            water+=max(0,min(left_max[i],right_max[i])-height[i])
+            if height[i]<left_max[i] and height[i]<right_max[i]: # that time only water store in that place
+                water+=(min(left_max[i],right_max[i])-height[i])
         return water
