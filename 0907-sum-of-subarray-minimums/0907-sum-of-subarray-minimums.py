7cfb5392f6af7d8aa331d46ae54487bcc9371c43
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2026-01-14 20:59:38 +0530

    Time: 119 ms (18.77%), Space: 23.9 MB (12.01%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.py
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.py
@@ -1,32 +1,31 @@
 class Solution:
     def sumSubarrayMins(self, arr: List[int]) -> int:
-        # “For every element, count how many subarrays it is the minimum of.”
         MOD=10**9+7
-        res=0
         n=len(arr)
-        def findNSE():# next smaller element
-            stack=[]
-            nse=[n]*n
-            for i in range(n):
-                while stack and arr[stack[-1]]>arr[i]:
-                    idx=stack.pop()
-                    nse[idx]=i
-                stack.append(i)
-            return nse
-        def findPSEE(): #previous smaller or equal elements
-            stack=[]
+        def findPSEE():
             pse=[-1]*n
-            for i in range(n):
+            stack=[]
+            for i in range(len(arr)):
                 while stack and arr[stack[-1]]>arr[i]:
                     stack.pop()
                 if stack:
                     pse[i]=stack[-1]
                 stack.append(i)
             return pse
-        # refer striver
-        pse=findPSEE()
+            
+        def findNSE():
+            nse=[n]*n
+            stack=[]
+            for i in range(n):
+                while stack and arr[stack[-1]]>arr[i]:
+                    idx=stack.pop()
+                    nse[idx]=i
+                stack.append(i)
+            return nse
         nse=findNSE()
-        for i in range(len(arr)):
+        pse=findPSEE()
+        res=0
+        for i in range(n):
             left=i-pse[i]
             right=nse[i]-i
             res=(res+(arr[i]*left*right)%MOD)%MOD
