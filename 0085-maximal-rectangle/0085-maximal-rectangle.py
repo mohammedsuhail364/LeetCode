3bb0ce4596b724ad068ceea26eac21d4a08bfce1
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2026-01-14 14:51:15 +0530

    Time: 43 ms (33.41%), Space: 24.9 MB (7.6%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.py
+++ 0085-maximal-rectangle/0085-maximal-rectangle.py
@@ -1,27 +1,44 @@
 class Solution:
     def maximalRectangle(self, matrix: List[List[str]]) -> int:
-        def largestRectangleInHistogram(heights):
-            max_area=0
+        temp=[]
+        for li in matrix:
+            if temp:
+                tmp=list(map(int,li))
+                tmp1=temp[-1]
+                for i in range(len(tmp)):
+                    if tmp[i]:
+                        tmp[i]=tmp1[i]+tmp[i]
+                temp.append(tmp)
+            else:
+                temp.append(list(map(int,li)))
+        def findNSE(nums):
             stack=[]
-            n=len(heights)
-            for i in range(n):
-                start=i
-                while stack and stack[-1][-1]>heights[i]:
-                    index,height=stack.pop()
-                    max_area=max(max_area,height*(i-index))
-                    start=index
-                stack.append((start,heights[i]))
-            for i,h in stack:
-                max_area=max(max_area,h*(n-i))
-            return max_area
-        rows,cols=len(matrix),len(matrix[0])
-        heights=[0]*cols
+            nse=[len(nums)]*len(nums)
+            for i in range(len(nums)):
+                while stack and nums[stack[-1]]>nums[i]:
+                    idx=stack.pop()
+                    nse[idx]=i
+                stack.append(i)
+            return nse
+        def findPSEE(nums):
+            stack=[]
+            pse=[-1]*len(nums)
+            for i in range(len(nums)):
+                while stack and nums[stack[-1]]>nums[i]:
+                    stack.pop()
+                if stack:
+                    pse[i]=stack[-1]
+                stack.append(i)
+            return pse
         res=0
-        for row in matrix:
-            for c in range(cols):
-                if row[c]=='1':
-                    heights[c]+=1
-                else:
-                    heights[c]=0
-            res=max(res,largestRectangleInHistogram(heights))
+        for li in temp:
+            nse=findNSE(li)
+            pse=findPSEE(li)
+            for i in range(len(li)):
+                width=nse[i]-pse[i]-1
+                height=li[i]
+                res=max(res,width*height)
         return res
+
+[["0","1"],
+ ["1","0"]]
