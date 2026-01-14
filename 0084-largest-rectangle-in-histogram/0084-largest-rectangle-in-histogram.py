7352468a160ea9da59fcfae14484c9f0a0ed8c1c
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2026-01-14 18:13:31 +0530

    Time: 114 ms (94.05%), Space: 36.7 MB (10.12%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
@@ -1,33 +1,16 @@
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
-        # find next smaller element
+        # refer neetcode
         n=len(heights)
-        def findNSE():
-            stack=[]
-            nse=[n]*n
-            for i in range(len(heights)):
-                while stack and heights[stack[-1]]>heights[i]:
-                    idx=stack.pop()
-                    nse[idx]=i
-                stack.append(i)
-            return nse
-        def findPSEE(): # find the previous smaller or equal element
-            stack=[]
-            pse=[-1]*n
-            for i in range(n):
-                while stack and heights[stack[-1]]>heights[i]:
-                    stack.pop()
-                if stack:
-                    pse[i]=stack[-1]
-                stack.append(i)
-            return pse
-        nse=findNSE()
-        pse=findPSEE()
-        res=0
+        max_area=0
+        stack=[]
         for i in range(n):
-            width=nse[i]-pse[i]-1
-            height=heights[i]
-            area=width*height
-            res=max(res,area)
-        return res
-        
+            start=i
+            while stack and stack[-1][-1]>heights[i]: # that means top of the stack is invalid it cannot move further we can calculate the res for the top of the stack
+                idx,h=stack.pop()
+                max_area=max(max_area,h*(i-idx)) # refers width
+                start=idx # this line tells current index is start at the idx why ? because heights[idx]>heights[i] so possibly it can extend left also that is the indication
+            stack.append((start,heights[i]))
+        for i,h in stack:
+            max_area=max(max_area,h*(n-i)) # refers width
+        return max_area
