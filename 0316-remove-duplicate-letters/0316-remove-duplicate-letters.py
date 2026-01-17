50b6f62eb94f6837293e88dd2dc2329a80376551
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2026-01-17 15:40:55 +0530

    Time: 2 ms (75.14%), Space: 19.4 MB (9.11%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.py
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.py
@@ -1,18 +1,13 @@
 class Solution:
     def removeDuplicateLetters(self, s: str) -> str:
-        # “When is it safe to REMOVE a character I already took?”
+        last_index={c:i for i, c in enumerate(s)}
         stack=[]
         seen=set()
-        last_index={c:i for i,c in enumerate(s)} # this map ensure the last index of every character
-        for i,c in enumerate(s):
-            if c in seen:
-                continue # this line ensures the existing character is te correct one
-            while stack and stack[-1] > c and i<last_index[stack[-1]]:
-                # when we can pop the element
-                # Current character < stack top (improves lexicographic order)
-                # The stack top appears later again in the string
-                # Removing it won’t lose uniqueness
+        for i in range(len(s)):
+            if s[i] in seen:
+                continue
+            while stack and stack[-1]>s[i] and i<last_index[stack[-1]]:
                 seen.remove(stack.pop())
-            stack.append(c)
-            seen.add(c)
+            stack.append(s[i])
+            seen.add(s[i])
         return ''.join(stack)
