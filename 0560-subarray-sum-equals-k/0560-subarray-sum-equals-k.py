eb694bae0a04c37e7b8b46eb84ba39b8ae596b7a
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2026-01-22 20:40:21 +0530

    Time: 35 ms (44.38%), Space: 22.1 MB (11%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,15 +1,16 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        n=len(nums)
+        prefix_sum={0:1} # empty subarray has 0
         res=0
-        seen={0:1}
-        cur=0
+        prefix_i=0
         for n in nums:
-            cur+=n
-            rem=cur-k
-            if rem in seen:
-                res+=seen[rem]
-            if cur not in seen:
-                seen[cur]=0
-            seen[cur]+=1
+            prefix_i+=n # this indicates the current sum prefix[i]
+            prefix_j=prefix_i-k # this indicates prefix[i] - prefix[j] = k if we subtract the prefix of j we get the k value
+            if prefix_j in prefix_sum:
+                res+=prefix_sum[prefix_j]
+            if prefix_i not in prefix_sum:
+                prefix_sum[prefix_i]=0
+            prefix_sum[prefix_i]+=1
         return res
+            
+            
