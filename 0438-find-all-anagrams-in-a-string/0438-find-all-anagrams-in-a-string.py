4393ede67069599c68a1d0e2876a0937f0ca7deb
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2026-01-22 21:10:31 +0530

    Time: 60 ms (43.83%), Space: 20 MB (11.77%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -1,16 +1,17 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        p_freq=Counter(p)
-        s_freq=defaultdict(int)
+        p_count=Counter(p)
+        s_count=defaultdict(int)
         l=0
+        
         res=[]
         for r in range(len(s)):
-            s_freq[s[r]]+=1
+            s_count[s[r]]+=1
             if (r-l+1)>len(p):
-                s_freq[s[l]]-=1
-                if s_freq[s[l]]==0:
-                    del s_freq[s[l]]
+                s_count[s[l]]-=1
+                if s_count[s[l]]==0:
+                    del s_count[s[l]]
                 l+=1
-            if p_freq==s_freq:
+            if p_count==s_count:
                 res.append(l)
         return res
