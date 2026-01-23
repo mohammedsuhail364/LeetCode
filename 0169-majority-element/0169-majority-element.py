9a149a19332001a229172e94257acc851b9ac4f7
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2026-01-23 14:43:25 +0530

    Time: 3 ms (84.58%), Space: 21.2 MB (15.35%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,8 +1,8 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        freq=Counter(nums)
-        target=int(len(nums)/2)
-        for v,f in freq.items():
-            if f>target:
-                return v
-        return -1
+        res=majority=0
+        for n in nums:
+            if majority==0:
+                res=n
+            majority+=(1 if n==res else -1)
+        return res
