30132c6e45a27b169b37861d4c49438649d4f008
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2026-01-27 18:08:48 +0530

    Time: 39 ms (32.2%), Space: 21.9 MB (28.75%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,16 +1,14 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        prefix_sum={0:1} # empty subarray has 0
+        pre_sub_sums={0:1} # empty subarray equals 0 this is for one edge case like if we reach the k at without subtract previous so we handle that 
+        cur_sum=0
         res=0
-        prefix_i=0
         for n in nums:
-            prefix_i+=n # this indicates the current sum prefix[i]
-            prefix_j=prefix_i-k # this indicates prefix[i] - prefix[j] = k if we subtract the prefix of j we get the k value
-            if prefix_j in prefix_sum:
-                res+=prefix_sum[prefix_j]
-            if prefix_i not in prefix_sum:
-                prefix_sum[prefix_i]=0
-            prefix_sum[prefix_i]+=1
+            cur_sum+=n
+            target=cur_sum-k # if cur_sum-k in the pre_sub_sum means if we subtract that subarray we can get the K
+            if target in pre_sub_sums:
+                res+=pre_sub_sums[target]
+            if cur_sum not in pre_sub_sums:
+                pre_sub_sums[cur_sum]=0
+            pre_sub_sums[cur_sum]+=1 # add this cur_sum for the future purpose
         return res
-            
-            
