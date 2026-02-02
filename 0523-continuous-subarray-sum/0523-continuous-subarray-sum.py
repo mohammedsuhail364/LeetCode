143812c0af68e616b45e29b0e9302c54f69a6403
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2026-02-02 21:31:11 +0530

    Time: 59 ms (26.58%), Space: 39.5 MB (49.14%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -1,15 +1,13 @@
+from typing import List
 class Solution:
-    def checkSubarraySum(self, nums, k: int) -> bool:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        pre_sum={0:-1}
         cur_sum=0
-        seen={0:-1}
         for r in range(len(nums)):
             cur_sum+=nums[r]
-            cur_sum=cur_sum%k
-            if cur_sum in seen:
-                if r - seen[cur_sum]>=2:
-                    return True
-            else:
-                seen[cur_sum]=r
+            target=cur_sum%k
+            if target in pre_sum and r - pre_sum[target] > 1:
+                return True
+            if target not in pre_sum:
+                pre_sum[target]=r
         return False
-
-
