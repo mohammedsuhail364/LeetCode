f5e228734b6c3da9e5658d57fd5b143fd4533a60
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2026-02-03 21:28:15 +0530

    Time: 147 ms (22.81%), Space: 25 MB (35.94%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
@@ -1,14 +1,14 @@
 class Solution:
-    def numberOfSubarrays(self, nums, k: int) -> int:
-        def count_subarray_less_than_k(k):
+    def numberOfSubarrays(self, nums: List[int], k: int) -> int:
+        def findSubarrayLessThanOrEqualToK(k):
+            res=0
             l=0
             odd=0
-            res=0
             for r in range(len(nums)):
-                odd+=nums[r]%2
-                while odd>k:
-                    odd-=nums[l]%2
+                odd+=(nums[r]%2)
+                while l<=r and odd>k:
+                    odd-=(nums[l]%2)
                     l+=1
-                res+=(r-l)
+                res+=(r-l+1)
             return res
-        return count_subarray_less_than_k(k)-count_subarray_less_than_k(k-1)
+        return findSubarrayLessThanOrEqualToK(k)-findSubarrayLessThanOrEqualToK(k-1)
