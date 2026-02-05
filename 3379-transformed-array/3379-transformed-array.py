a8be782a29b8c4d22490dd35cd4ac92914b94d38
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2026-02-05 15:23:59 +0530

    Time: 70 ms (7.17%), Space: 19.4 MB (34.53%) - LeetHub

---
+++ 3379-transformed-array/3379-transformed-array.py
@@ -0,0 +1,9 @@
+class Solution:
+    def constructTransformedArray(self, nums: List[int]) -> List[int]:
+        res=[0]*len(nums)
+        for i in range(len(nums)):
+            if nums[i]>0:
+                res[i]=nums[(i+nums[i])%len(nums)]
+            elif nums[i]<0:
+                res[i]=nums[(i-abs(nums[i]))%len(nums)]
+        return res
