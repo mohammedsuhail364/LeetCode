0c9d55012b4e8cc9e9546d63ae9f9a6f915a15b1
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2026-02-05 17:16:58 +0530

    Time: 14 ms (96.39%), Space: 25.7 MB (31.75%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,13 +1,15 @@
+from typing import List
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        n=len(nums)
-        res=[1]*n
-        pre=1
-        suf=1
-        for i in range(n):
-            res[i]=pre
-            pre*=nums[i]
-        for i in range(n-1,-1,-1):
-            res[i]*=suf
-            suf*=nums[i]
+        #  find the prefix multiplication
+        prefix=1 # default case when i = 0 
+        res=[0]*len(nums)
+        for i in range(len(nums)):
+            res[i]=prefix
+            prefix=prefix*nums[i]
+        
+        suffix=1
+        for i in range(len(nums)-1,-1,-1):
+            res[i]=res[i]*suffix
+            suffix=suffix*nums[i]
         return res
