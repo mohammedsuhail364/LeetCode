1563e5b241cc10ac956bc4c6206bfe36175880ae
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2026-02-09 15:09:10 +0530

    Time: 3 ms (82.81%), Space: 20 MB (46.25%) - LeetHub

--- 0986-interval-list-intersections/0986-interval-list-intersections.py
+++ 0986-interval-list-intersections/0986-interval-list-intersections.py
@@ -1,15 +1,16 @@
-from typing import List
 class Solution:
     def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
         i=0
         j=0
         res=[]
-        while i<len(firstList) and j<len(secondList):    
-            s=max(firstList[i][0],secondList[j][0])
-            e=min(firstList[i][1],secondList[j][1])
-            if s<=e:
-                res.append([s,e])
-            if firstList[i][1]<secondList[j][1]:
+        while i<len(firstList) and j<len(secondList):
+            first=firstList[i]
+            second=secondList[j]
+            
+            temp=[max(first[0],second[0]),min(first[1],second[1])]
+            if temp[0]<=temp[1]:
+                res.append(temp)
+            if first[1]<second[1]:
                 i+=1
             else:
                 j+=1
