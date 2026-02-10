76b7e94088cea0eacd57832c63fd404c2bf9f914
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2026-02-10 20:56:39 +0530

    Time: 223 ms (27.33%), Space: 44.1 MB (25.63%) - LeetHub

--- 2251-number-of-flowers-in-full-bloom/2251-number-of-flowers-in-full-bloom.py
+++ 2251-number-of-flowers-in-full-bloom/2251-number-of-flowers-in-full-bloom.py
@@ -1,19 +1,40 @@
 class Solution:
     def fullBloomFlowers(self, flowers: List[List[int]], people: List[int]) -> List[int]:
         # can we solve with two different approach 
-        # 1. using heap
-        # 2. using sweep line but in this using so many imports(not built-in) thats why i m prefer the heap solution
-        people=[(p,i) for i,p in enumerate(people)]
-        flowers.sort()
-        end=[]
-        count=0
-        res=[0]*len(people)
-        j=0
-        for p,i in sorted(people):
-            while j<len(flowers) and flowers[j][0]<=p:
-                heappush(end,flowers[j][1])
-                j+=1
-            while end and end[0]<p:
-                heappop(end)
-            res[i]=len(end)
+        # 1. using heap -> refer neetcode
+        # 2. using sweep line -> refer editorial
+        diff={} # default case we see 0 new flowers at 0 time
+        def getRightMostElement(nums,target):
+            l=0
+            r=len(nums)-1
+            res=-1
+            while l<=r:
+                m=(l+r)//2
+                if nums[m]<=target:
+                    res=m
+                    l=m+1
+                else:
+                    r=m-1
+            return res
+
+        for s,e in flowers:
+            diff[s]=diff.get(s,0)+1
+            diff[e+1]=diff.get(e+1,0)-1
+
+        prefix=[]
+        positions=[]
+        cur=0
+        for key,val in sorted(diff.items()):
+            positions.append(key)
+            cur+=val
+            prefix.append(cur)
+        res=[]
+        for p in people:
+            idx=getRightMostElement(positions,p)
+            if idx==-1:
+                res.append(0)
+            else:
+                res.append(prefix[idx])
         return res
+
+        
