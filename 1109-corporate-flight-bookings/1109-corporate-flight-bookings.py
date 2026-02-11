55ecc1cc3dc6995ed924632dc2b3e6b6c114df98
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2026-02-11 20:10:04 +0530

    Time: 18 ms (94.59%), Space: 31.2 MB (17.82%) - LeetHub

--- 1109-corporate-flight-bookings/1109-corporate-flight-bookings.py
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.py
@@ -1,10 +1,10 @@
 class Solution:
     def corpFlightBookings(self, bookings: List[List[int]], n: int) -> List[int]:
-        res=[0]*n
+        diff=[0]*n
         for s,e,inc in bookings:
-            res[s-1]+=inc
+            diff[s-1]+=inc
             if e<n:
-                res[e]-=inc
-        for i in range(1,len(res)):
-            res[i]=res[i-1]+res[i]
-        return res
+                diff[e]-=inc
+        for i in range(1,len(diff)):
+            diff[i]+=diff[i-1]
+        return diff
