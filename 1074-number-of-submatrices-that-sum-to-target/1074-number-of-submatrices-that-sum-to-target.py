02493611de5e86d71eecb77e4768e9681f89676a
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2026-02-11 21:13:23 +0530

    Time: 571 ms (35.57%), Space: 20.3 MB (13.81%) - LeetHub

--- 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.py
+++ 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.py
@@ -1,23 +1,22 @@
 class Solution:
     def numSubmatrixSumTarget(self, matrix: List[List[int]], target: int) -> int:
-        # refer neetcode
-        ROWS=len(matrix)
+        ROWS=len(matrix) 
         COLS=len(matrix[0])
         pre_sum=[[0]*COLS for i in range(ROWS)]
-        res=0
         for r in range(ROWS):
             for c in range(COLS):
-                top=pre_sum[r-1][c] if r>0 else 0
+                diag=pre_sum[r-1][c-1] if r>0 and c>0 else 0
+                up=pre_sum[r-1][c] if r>0 else 0
                 left=pre_sum[r][c-1] if c>0 else 0
-                top_left=pre_sum[r-1][c-1] if min(r,c)>0 else 0
-                pre_sum[r][c]=matrix[r][c]+top+left-top_left
+                pre_sum[r][c]=matrix[r][c]+up+left-diag
+        res=0
         for r1 in range(ROWS):
             for r2 in range(r1,ROWS):
                 count=defaultdict(int)
                 count[0]=1
                 for c in range(COLS):
-                    cur_sum=pre_sum[r2][c]-(pre_sum[r1-1][c] if r1>0 else 0)
-                    diff = cur_sum - target
+                    cur_sum=pre_sum[r2][c] - (pre_sum[r1-1][c] if r1>0 else 0)
+                    diff=cur_sum-target
                     res+=count[diff]
                     count[cur_sum]+=1
         return res
