52905315700cdd01512e156aa314227449e91585
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2026-02-12 20:06:48 +0530

    Time: 331 ms (18.37%), Space: 19.2 MB (38.78%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,15 +1,9 @@
 class Solution:
     def fib(self, n: int) -> int:
-        if n==1 or n==0:
-            return n
-        first=0
-        second=1
-        third=0
-        for i in range(n-1):
-            third=first+second
-            first=second
-            second=third
-        return third
-        # 0 1 1 2
+        def dfs(n):
+            if n==1 or n==0:
+                return n
+            return dfs(n-1)+dfs(n-2)
+        return dfs(n)
 
 
