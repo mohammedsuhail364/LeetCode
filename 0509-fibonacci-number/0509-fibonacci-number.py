c90aa7889ffe59c9dcb84bf1574a5aafd9599a63
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2026-02-12 20:07:29 +0530

    Time: 43 ms (65.9%), Space: 19.3 MB (38.78%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,9 +1,13 @@
 class Solution:
     def fib(self, n: int) -> int:
+        memo={}
         def dfs(n):
+            if n in memo:
+                return memo[n]
             if n==1 or n==0:
                 return n
-            return dfs(n-1)+dfs(n-2)
+            memo[n] = dfs(n-1)+dfs(n-2)
+            return memo[n]
         return dfs(n)
 
 
