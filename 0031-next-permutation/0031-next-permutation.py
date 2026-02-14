6ea59e3302fd1b9445673070840abf3dd3a94274
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2026-02-14 18:31:32 +0530

    Time: 3 ms (6.24%), Space: 19.5 MB (20.36%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -3,25 +3,28 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        
+        # this question was very interesting 
+        # always remember permutation always looks like this 123 < 132 < 213 < 231 < 312 < 321
+        # increasing order
+        # if we want to find after the last one we want to return first one 
+        # simple trick happend there 
+        # eg : 2154300
+        # 1. find the non decreasing order from backwards
+        # 2. swap the smallest one after the break
+        # 3. reverse the slicing part
         n=len(nums)
         i=n-2
         while True:
-            if i>=0 and nums[i]<nums[i+1]:
-                break
-            if i<0:
+            if i<0 or (i>=0 and nums[i]<nums[i+1]):
                 break
             i-=1
         if i>=0:
             j=n-1
+            # find the smallest one after that break point
             while True:
-                if j>=0 and nums[i]<nums[j]:
-                    break
-                if j<0:
+                if j<0 or (j>=0 and nums[i]<nums[j]):
                     break
                 j-=1
+            # swap the small number after the break point and break point
             nums[i],nums[j]=nums[j],nums[i]
         nums[i+1:]=nums[i+1:][::-1]
-    
-
-        
