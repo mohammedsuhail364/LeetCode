d403947e5e78a7afe90d0b468239f337ca1dcafa
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2026-02-16 20:32:20 +0530

    Time: 30 ms (5.16%), Space: 19.5 MB (23.97%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.py
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -4,19 +4,21 @@ class Solution:
         def dfs(i,path):
             if i==len(s):
                 if len(path)==4:
-                    path=''.join(path)
-                    res.append(path[:-1])
+                    path='.'.join(path)
+                    res.append(path)
                 return
-            for j in range(i,len(s)):
+            for j in range(i,i+4): # because ip <=255
+                if j>len(s):
+                    break
                 if self.isValid(s,i,j):
-                    path.append(s[i:j+1]+'.')
+                    path.append(s[i:j+1])
                     dfs(j+1,path)
                     path.pop()
         dfs(0,[])
         return res
     def isValid(self,s,i,j):
         val=s[i:j+1]
-        if int(val)>255 or val in '@' or (len(val)>1 and val[0]=='0'):
+        if (val and int(val)>255 )or (len(val)>1 and val[0]=='0'):
             return False
         return True
         
