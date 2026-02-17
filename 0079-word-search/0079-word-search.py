4f994ef01a2d9a811af8e9d338886bb0d86bfc54
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2026-02-17 21:25:03 +0530

    Time: 3464 ms (70.71%), Space: 19.3 MB (69.61%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,26 +1,29 @@
 class Solution:
-    [["C","A","A"],
-     ["A","A","A"],
-     ["B","C","D"]]
     def exist(self, board: List[List[str]], word: str) -> bool:
-        rows=len(board)
-        cols=len(board[0])
-        visit=set()
+        ROWS=len(board)
+        COLS=len(board[0])
+        visit=[[False]*COLS for r in range(ROWS)]
         def dfs(r,c,i):
             if i>=len(word):
                 return True
-            if r<0 or c<0 or r>=rows or c>=cols or (r,c) in visit: 
+            if r==ROWS or r<0 or c==COLS or c<0:
                 return False
-            if word[i]!=board[r][c]:
+            if board[r][c]!=word[i] or visit[r][c]:
                 return False
-            visit.add((r,c))
-            x=dfs(r+1,c,i+1) or dfs(r-1,c,i+1) or dfs(r,c+1,i+1) or dfs(r,c-1,i+1)
-            visit.remove((r,c))
-            return x
-        
-        for r in range(rows):
-            for c in range(cols):
-                if board[r][c]==word[0]:
-                    if dfs(r,c,0):
-                        return True
+
+            visit[r][c]=True
+            res= (
+            dfs(r+1,c,i+1) or 
+            dfs(r-1,c,i+1) or 
+            dfs(r,c+1,i+1) or 
+            dfs(r,c-1,i+1)
+            )
+            visit[r][c]=False
+            return res
+            
+            
+        for r in range(ROWS):
+            for c in range(COLS):
+                if dfs(r,c,0):
+                    return True
         return False
