047778edff4beb6508f314a26bf2e819a33cffd7
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2026-02-17 21:12:14 +0530

    Time: 0 ms (100%), Space: 19.4 MB (60.29%) - LeetHub

--- 0401-binary-watch/0401-binary-watch.py
+++ 0401-binary-watch/0401-binary-watch.py
@@ -1,10 +1,11 @@
 class Solution:
-    def readBinaryWatch(self, n: int) -> List[str]:
+    def readBinaryWatch(self, turnedOn: int) -> List[str]:
         res=[]
         for h in range(12):
+            h_count=bin(h).count('1')
             for m in range(60):
-                h_ones=bin(h).count('1')
-                m_ones=bin(m).count('1')
-                if h_ones+m_ones==n:
+                m_count=bin(m).count('1')
+                if h_count+m_count==turnedOn:
                     res.append(f"{h}:{m:02d}")
         return res
+                
