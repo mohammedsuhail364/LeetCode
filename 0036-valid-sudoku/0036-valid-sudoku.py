3b21706e3dbfaeccd6cfa4f429a3a9067cc6b061
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2026-02-18 21:41:54 +0530

    Time: 6 ms (36.52%), Space: 19.5 MB (27.86%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,16 +1,19 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        row=defaultdict(set)
-        col=defaultdict(set)
-        sub_box=defaultdict(set)
-        for r in range(len(board)):
-            for c in range(len(board[0])):
-                if board[r][c]=='.':
-                    continue
-                value=board[r][c]
-                if value in row[r] or value in col[c] or value in sub_box[(r//3,c//3)]:
+        ROWS=len(board)
+        COLS=len(board[0])
+        row_set=defaultdict(set)
+        col_set=defaultdict(set)
+        box=defaultdict(set)
+        for r in range(ROWS):
+            for c in range(COLS):
+                val=board[r][c]
+                if board[r][c]!='.' and (val in row_set[r] or val in col_set[c] or val in box[(r//3,c//3)]):
                     return False
-                row[r].add(value)
-                col[c].add(value)
-                sub_box[(r//3,c//3)].add(value)
+
+                if board[r][c]!='.':
+                    row_set[r].add(val)
+                    col_set[c].add(val)
+                    box[(r//3,c//3)].add(val)
         return True
+                
