dc43362811cb5246e658d14f0e2d154be7137280
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2026-02-19 21:39:10 +0530

    Time: 51 ms (88.21%), Space: 19.4 MB (74.15%) - LeetHub

--- 0696-count-binary-substrings/0696-count-binary-substrings.py
+++ 0696-count-binary-substrings/0696-count-binary-substrings.py
@@ -1,5 +1,23 @@
 class Solution:
     def countBinarySubstrings(self, s: str) -> int:
+        # s = "00110011"
+        # Groups:
+
+        # "00" → 2
+        # "11" → 2
+        # "00" → 2
+        # "11" → 2
+        # So group lengths:
+
+        # [2, 2, 2, 2]
+        # Number of valid substrings =
+        # min(group[i], group[i+1]) summed for all adjacent groups.
+
+        # So:
+
+        # min(2,2) + min(2,2) + min(2,2)
+        # = 2 + 2 + 2
+        # = 6
         res=0
         cur=1
         prev=0
