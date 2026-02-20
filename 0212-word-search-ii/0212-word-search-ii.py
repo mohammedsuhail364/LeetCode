335211adb1514ed7f1de74732a830470a5f47be6
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2026-02-20 20:18:13 +0530

    Time: 6224 ms (32.4%), Space: 21 MB (59.45%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.py
+++ 0212-word-search-ii/0212-word-search-ii.py
@@ -1,36 +1,44 @@
-class TrieNode:
+class Trie:
     def __init__(self):
         self.children={}
-        self.isLastWord=False
+        self.isLast=False
     def add_word(self,word):
         cur=self
         for c in word:
             if c not in cur.children:
-                cur.children[c]=TrieNode()
+                cur.children[c]=Trie()
             cur=cur.children[c]
-        cur.isLastWord=True
+        cur.isLast=True
 
+
+                
 class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
-        root=TrieNode()
-        for w in words:
-            root.add_word(w)
-        visited,res=set(),set()
+        # first add the words as Trie
+        root=Trie()
+        res=set()
+        visited=set()
+        for word in words:
+            root.add_word(word)
         def dfs(r,c,node,word):
-            if(r<0 or c<0 or r==rows or c==cols or (r,c) in visited or board[r][c] not in node.children):
+            if r<0 or c<0 or c==COLS or r==ROWS or (r,c) in visited or board[r][c] not in node.children:
                 return
             visited.add((r,c))
             node=node.children[board[r][c]]
             word+=board[r][c]
-            if node.isLastWord:
+            if node.isLast:
                 res.add(word)
-            dfs(r-1,c,node,word)
             dfs(r+1,c,node,word)
-            dfs(r,c-1,node,word)
+            dfs(r-1,c,node,word)
             dfs(r,c+1,node,word)
+            dfs(r,c-1,node,word)
             visited.remove((r,c))
-        rows,cols=len(board),len(board[0])
-        for r in range(rows):
-            for c in range(cols):
+        ROWS=len(board)
+        COLS=len(board[0])
+        for r in range(ROWS):
+            for c in range(COLS):
                 dfs(r,c,root,"")
+
         return list(res)
+
+        
