f913b445e6f61c309dfd108e7a6e7e6073fcf504
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2026-02-20 20:52:56 +0530

    Time: 1044 ms (46.72%), Space: 68.5 MB (42.83%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
@@ -1,38 +1,33 @@
-class TrieNode:
-    def __init__(self):
-        self.children={}
-        self.last=False
-
 class WordDictionary:
 
     def __init__(self):
-        self.root=TrieNode()
+        self.children={}
+        self.isLast=False
 
     def addWord(self, word: str) -> None:
-        cur=self.root
+        cur=self
         for c in word:
             if c not in cur.children:
-                cur.children[c]=TrieNode()
+                cur.children[c]=WordDictionary()
             cur=cur.children[c]
-        cur.last=True
+        cur.isLast=True
 
     def search(self, word: str) -> bool:
-        def dfs(j,root):
-            cur=root
-            for i in range(j,len(word)):
-                c=word[i]
-                if c =='.':
-                    for child in cur.children.values():
-                        if dfs(i+1,child):
-                            return True
-                    return False
-                else:
-                    if c not in cur.children:
+            root=self
+            def dfs(j,cur):
+                for i in range(j,len(word)):
+                    if word[i]=='.':
+                        for c in cur.children.values():
+                            if dfs(i+1,c):
+                                return True
                         return False
-                    cur=cur.children[c]
-            return cur.last
-        return dfs(0,self.root)
-        
+                    else:
+                        if word[i] not in cur.children:
+                            return False
+                        cur=cur.children[word[i]]
+                return cur.isLast
+            return dfs(0,root)
+                
 
 
 # Your WordDictionary object will be instantiated and called as such:
