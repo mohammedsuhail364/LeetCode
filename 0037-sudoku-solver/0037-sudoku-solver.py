6283aea844d4a62d8c0997e0f1032048760ad78e
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2026-02-23 20:30:48 +0530

    Time: 2909 ms (21.5%), Space: 19.8 MB (8.14%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.py
+++ 0037-sudoku-solver/0037-sudoku-solver.py
@@ -6,33 +6,31 @@ class Solution:
         row_set=defaultdict(set)
         col_set=defaultdict(set)
         sub_boxes=defaultdict(set)
-        def dfs(r,c):
-            if c==9:
-                return dfs(r+1,0)
-            if r==9:
+        def dfs(idx):
+            if idx==len(empties):
                 return True
-            if board[r][c]!='.':
-                return dfs(r,c+1)
+            r,c=empties[idx]
             for i in map(str,range(1,10)):
                 if i not in row_set[r] and i not in col_set[c] and i not in sub_boxes[r//3,c//3]:
                     board[r][c]=i
                     row_set[r].add(i)
                     col_set[c].add(i)
                     sub_boxes[r//3,c//3].add(i)
-                    if dfs(r,c+1):
+                    if dfs(idx+1):
                         return True
                     board[r][c]='.'
                     row_set[r].remove(i)
                     col_set[c].remove(i)
                     sub_boxes[r//3,c//3].remove(i)
             return False
-
+        empties=[]
         for r in range(9):
             for c in range(9):
                 if board[r][c]!='.':
                     row_set[r].add(board[r][c])
                     col_set[c].add(board[r][c])
                     sub_boxes[r//3,c//3].add(board[r][c])
-        
-        dfs(0,0)
+                else:
+                    empties.append((r,c))
+        dfs(0)
         
