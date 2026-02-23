a4256da3d54bddc7e7d25bf07273c3edd9b64fc5
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2026-02-23 20:20:35 +0530

    Time: 3017 ms (14.5%), Space: 19.8 MB (22.75%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.py
+++ 0037-sudoku-solver/0037-sudoku-solver.py
@@ -3,44 +3,36 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        def backtrack(r,c):
+        row_set=defaultdict(set)
+        col_set=defaultdict(set)
+        sub_boxes=defaultdict(set)
+        def dfs(r,c):
             if c==9:
-                return backtrack(r+1,0)
+                return dfs(r+1,0)
             if r==9:
                 return True
             if board[r][c]!='.':
-                return backtrack(r,c+1)
-            for num in map(str,range(1,10)):
-                if num in row_val[r] or num in col_val[c] or num in sub_box[(r//3,c//3)]:
-                    continue
-                board[r][c]=num
-                row_val[r].add(num)
-                col_val[c].add(num)
-                sub_box[(r//3,c//3)].add(num)
-                if backtrack(r,c+1):
-                    return True
-                # backtrack the number
-                board[r][c]='.'
-                row_val[r].remove(num)
-                col_val[c].remove(num)
-                sub_box[(r//3,c//3)].remove(num)
+                return dfs(r,c+1)
+            for i in map(str,range(1,10)):
+                if i not in row_set[r] and i not in col_set[c] and i not in sub_boxes[r//3,c//3]:
+                    board[r][c]=i
+                    row_set[r].add(i)
+                    col_set[c].add(i)
+                    sub_boxes[r//3,c//3].add(i)
+                    if dfs(r,c+1):
+                        return True
+                    board[r][c]='.'
+                    row_set[r].remove(i)
+                    col_set[c].remove(i)
+                    sub_boxes[r//3,c//3].remove(i)
             return False
 
-        rows=len(board)
-        cols=len(board[0])
-        row_val=defaultdict(set)
-        col_val=defaultdict(set)
-        sub_box=defaultdict(set)
-        for r in range(rows):
-            for c in range(cols):
-                if board[r][c]=='.':
-                    continue
-                row_val[r].add(board[r][c])
-                col_val[c].add(board[r][c])
-                sub_box[(r//3,c//3)].add(board[r][c])
+        for r in range(9):
+            for c in range(9):
+                if board[r][c]!='.':
+                    row_set[r].add(board[r][c])
+                    col_set[c].add(board[r][c])
+                    sub_boxes[r//3,c//3].add(board[r][c])
         
-        backtrack(0,0)
-                    
-
-
+        dfs(0,0)
         
