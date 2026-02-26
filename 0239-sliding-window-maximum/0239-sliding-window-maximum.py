f2e24386c030dd85cf0a61b874889bd68be5bd7c
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2026-02-26 21:12:55 +0530

    Time: 189 ms (53.73%), Space: 34.5 MB (82.33%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -1,16 +1,14 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
         l=0
-        q=deque()
         res=[]
+        q=deque()
         for r in range(len(nums)):
-            # maintain the monotonic stack
             while q and nums[q[-1]]<nums[r]:
                 q.pop()
-            # remove the non valid left pointers  
+            q.append(r)
             if q and l>q[0]:
                 q.popleft()
-            q.append(r)
             if (r-l+1)>=k:
                 res.append(nums[q[0]])
                 l+=1
