875d217b0070e4721dab8ebd05c0dffcc0dfb019
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2026-03-08 20:52:33 +0530

    Time: 0 ms (100%), Space: 19.8 MB (15.55%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.py
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.py
@@ -1,18 +1,15 @@
 class Solution:
     def findDifferentBinaryString(self, nums: List[str]) -> str:
         seen=set(nums)
-        res=''
         sen="01"
-        def dfs(i,path):
-            nonlocal res
+        def dfs(path):
             if len(path)==len(nums[0]):
                 temp=''.join(path)
                 if temp not in seen:
-                    res=temp
-                return
-            if i>=len(sen):
-                return
-            dfs(i,path+[sen[i]])
-            dfs(i+1,path)
-        dfs(0,[])
-        return res
+                    return temp
+                return None
+            for bit in ["0","1"]:
+                res=dfs(path+[bit])
+                if res:
+                    return res
+        return dfs([])
