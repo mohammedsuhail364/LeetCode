04a4e7f868a536da08217b1bcf6187874824afda
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2026-03-22 09:25:20 +0530

    Time: 5 ms (25.53%), Space: 19.5 MB (19.2%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.py
+++ 0763-partition-labels/0763-partition-labels.py
@@ -1,13 +1,12 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        # in this question we observe one thing like if we take a first character 'a' it ends in index 8 that time we can include character from 0 - 8 to make a partition but one thing if the second or third letter has a last index is 10 or 12 that time we can include that also the same partition it is the main intuation 
-        last={s[i]:i for i in range(len(s))} # find the last occurrence of the characters
+        last_index={v:i for i,v in enumerate(s)}
         start=0
-        res=[]
         max_index=0
+        res=[]
         for i in range(len(s)):
-            max_index=max(max_index,last[s[i]])
-            if i == max_index:
-                res.append(i-start+1) # length of the word
+            max_index=max(max_index,last_index[s[i]])
+            if i==max_index:
+                res.append(i-start+1)
                 start=i+1
         return res
