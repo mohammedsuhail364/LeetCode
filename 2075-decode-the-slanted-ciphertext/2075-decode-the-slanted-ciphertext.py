e2072e811074144f7ea0634dbc5690886244d337
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2026-04-04 11:28:22 +0530

    Time: 211 ms (53.25%), Space: 32.3 MB (48.05%) - LeetHub

--- 2075-decode-the-slanted-ciphertext/2075-decode-the-slanted-ciphertext.py
+++ 2075-decode-the-slanted-ciphertext/2075-decode-the-slanted-ciphertext.py
@@ -1,26 +1,12 @@
 class Solution:
     def decodeCiphertext(self, encodedText: str, rows: int) -> str:
         cols=(len(encodedText)//rows)
-        curr_row=0
-        curr_col=0
-        r=0
-        c=0
-        res=''
-        encodedTextNew=[[] for _ in range(rows)]
-        curent_row=-1
-        for i in range(len(encodedText)):
-            if i%cols ==0:
-                curent_row+=1
-            encodedTextNew[curent_row].append(encodedText[i])
-            
-        
-        while curr_row<rows and curr_col<cols:
-            r=curr_row
-            c=curr_col
+        res=[]
+        for start_col in range(cols):
+            r=0
+            c=start_col
             while r<rows and c<cols:
-                res+=encodedTextNew[r][c]
+                res.append(encodedText[r*cols+c])
                 r+=1
                 c+=1
-            curr_row=0
-            curr_col+=1
-        return res.rstrip()
+        return ''.join(res).rstrip()
