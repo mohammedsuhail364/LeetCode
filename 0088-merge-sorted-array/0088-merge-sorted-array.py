353f59df36930a0a34ecffee806dbff5b6847b8b
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2026-04-05 15:49:21 +0530

    Time: 0 ms (100%), Space: 19.3 MB (85.52%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,6 +3,20 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        temp=sorted(nums1[:m]+nums2)
-        for i in range(len(temp)):
-            nums1[i]=temp[i]
+        # we can start from the last
+        last=m+n-1
+        m-=1
+        n-=1 # for index count
+        while m>=0 and n>=0:
+            if nums1[m]>nums2[n]:
+                nums1[last]=nums1[m]
+                m-=1
+            else:
+                nums1[last]=nums2[n]
+                n-=1
+            last-=1
+        while n>=0:
+            nums1[last]=nums2[n]
+            n-=1
+            last-=1
+        
