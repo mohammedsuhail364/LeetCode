125a13f174a4f20ce8c466c9a05c8ee76675304c
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2026-04-05 16:07:55 +0530

    Time: 187 ms (61.86%), Space: 35.5 MB (28.66%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -1,15 +1,17 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        # we can use the monotonic queue why because we can access in the two sides also 
+        q=deque()
         l=0
         res=[]
-        q=deque()
         for r in range(len(nums)):
             while q and nums[q[-1]]<nums[r]:
                 q.pop()
             q.append(r)
-            if q and l>q[0]:
+            if l>q[0]:
                 q.popleft()
-            if (r-l+1)>=k:
+            if r-l+1>=k:
                 res.append(nums[q[0]])
                 l+=1
         return res
+            
