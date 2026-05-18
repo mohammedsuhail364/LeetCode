5a79c8a07404075ef857c26e7a6ad47e0044b2ed
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2026-05-18 21:17:39 +0530

    Time: 15 ms (98.44%), Space: 28.7 MB (42.96%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,14 +1,11 @@
 from typing import List
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        n=len(prices)
-        next_greater=prices[:]
-        next_smaller=prices[:]
-        for i in range(n-2,-1,-1):
-            next_greater[i]=max(prices[i],next_greater[i+1])
-        for i in range(1,n):
-            next_smaller[i]=min(prices[i],next_smaller[i-1])
+        min_price=inf
         res=0
-        for i in range(n):
-            res=max(res,next_greater[i]-next_smaller[i])
+        for p in prices:
+            if p<min_price:
+                min_price=p
+            if p-min_price>res:
+                res=p-min_price
         return res
