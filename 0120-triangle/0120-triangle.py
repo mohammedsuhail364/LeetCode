6ab9a0999669182a33c4437331b0e3265b75eb24
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2026-05-20 20:36:42 +0530

    Time: 3 ms (69.8%), Space: 20.5 MB (25.21%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,16 +1,27 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
+        dp=[[inf]*(r+1) for r in range(len(triangle)+1)]
         ROWS=len(triangle)
-        cache={}
-        def dfs(r,c):
-            if (r,c) in cache:
-                return cache[r,c]
-            if r>=ROWS:
-                return inf
-            if c<0 or c>=len(triangle[r]):
-                return inf
+        # base case
+        for c in range(len(triangle[ROWS-1])):
+            dp[ROWS-1][c]=triangle[ROWS-1][c]
+        for r in range(ROWS-1,-1,-1):
             if r==ROWS-1:
-                return triangle[r][c]
-            cache[r,c] = triangle[r][c]+min(dfs(r+1,c+1),dfs(r+1,c))
-            return cache[r,c]
-        return dfs(0,0)
+                continue
+            for c in range(len(triangle[r])-1,-1,-1):
+                dp[r][c]=triangle[r][c]+min(dp[r+1][c+1],dp[r+1][c])
+        return dp[0][0]
+        # ROWS=len(triangle)
+        # cache={}
+        # def dfs(r,c):
+        #     if (r,c) in cache:
+        #         return cache[r,c]
+        #     if r>=ROWS:
+        #         return inf
+        #     if c<0 or c>=len(triangle[r]):
+        #         return inf
+        #     if r==ROWS-1:
+        #         return triangle[r][c]
+        #     cache[r,c] = triangle[r][c]+min(dfs(r+1,c+1),dfs(r+1,c))
+        #     return cache[r,c]
+        # return dfs(0,0)
