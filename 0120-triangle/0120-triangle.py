ca3468bf0686e002f6a59fee41c538d061d6edad
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2026-05-20 20:24:20 +0530

    Time: 14 ms (6.93%), Space: 23.4 MB (5.03%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,7 +1,16 @@
 class Solution:
-    def minimumTotal(self, triangle) -> int:
-        dp=[0]*(len(triangle)+1)
-        for row in triangle[::-1]:
-            for i,n in enumerate(row):
-                dp[i]=min(dp[i],dp[i+1])+n
-        return dp[0]
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        ROWS=len(triangle)
+        cache={}
+        def dfs(r,c):
+            if (r,c) in cache:
+                return cache[r,c]
+            if r>=ROWS:
+                return inf
+            if c<0 or c>=len(triangle[r]):
+                return inf
+            if r==ROWS-1:
+                return triangle[r][c]
+            cache[r,c] = triangle[r][c]+min(dfs(r+1,c+1),dfs(r+1,c))
+            return cache[r,c]
+        return dfs(0,0)
