364ba513bedce0117b0389c46db2c450791f35dc
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2026-05-21 20:50:23 +0530

    Time: 35 ms (84.04%), Space: 26.8 MB (5.8%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -1,20 +1,19 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        # refer neetcode
-        n=len(word1)
-        m=len(word2)
-        dp=[[0]*(m+1) for _ in range(n+1)]
-        for i in range(n):
-            dp[i][m]=n-i
-        for j in range(m):
-            dp[n][j]=m-j
-        for i in range(n-1,-1,-1):
-            for j in range(m-1,-1,-1):
-                if word1[i]==word2[j]:
-                    dp[i][j]=dp[i+1][j+1]
-                else:
-                    insert=dp[i][j+1]
-                    delete=dp[i+1][j]
-                    replace=dp[i+1][j+1]
-                    dp[i][j]=1+min(insert,delete,replace)
-        return dp[0][0]
+        cache={}
+        def dfs(i,j):
+            if (i,j) in cache:
+                return cache[i,j]
+            if j>=len(word2): # when j exceeds but i has a value we want to delete the chars in word1
+                return len(word1)-i
+            if i>=len(word1): # when i exceeds but j has a value we want to insert the chars in word1
+                return len(word2)-j
+            if word1[i]==word2[j]:
+                cache[i,j] = dfs(i+1,j+1)
+                return cache[i,j]
+            insert=1+dfs(i,j+1)
+            delete=1+dfs(i+1,j)
+            replace=1+dfs(i+1,j+1)
+            cache[i,j] = min(insert,delete,replace)
+            return cache[i,j]
+        return dfs(0,0)
