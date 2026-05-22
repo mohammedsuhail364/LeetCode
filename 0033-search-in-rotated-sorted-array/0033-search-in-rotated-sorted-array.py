a538f85da30c8e3c0d0d17655de678520a1703f4
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2026-05-22 20:23:04 +0530

    Time: 0 ms (100%), Space: 19.4 MB (39.04%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,23 +1,19 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+        n=len(nums)
         l=0
-        r=len(nums)-1
+        r=n-1
         while l<=r:
             m=(l+r)//2
             if nums[m]==target:
                 return m
-            # we can only check in the sorted position 
-            # first we can check if the left side is sorted or not
             if nums[l]<=nums[m]:
-                # we can check the target is in the left side
                 if nums[l]<=target<nums[m]:
-                    r=m-1 # move the right pointer to mid - 1 and check
+                    r=m
                 else:
-                    # in this time we know target is not in the left side so we can move the left pointer to mid+1
-                    l=m+1 
+                    l=m+1
             else:
-                # we can identify the right is sorted
-                if nums[m]<target<=nums[r]: # we can check the target is in the right side
+                if nums[m]<target<=nums[r]:
                     l=m+1
                 else:
                     r=m-1
