a0371f84599d4c29d97005070dfbb461197e685d
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2026-06-03 20:44:05 +0530

    Time: 2338 ms (10.62%), Space: 496.9 MB (5.13%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -3,15 +3,14 @@ class Solution:
         total=sum(nums)
         if total%2!=0:
             return False
-        target=total//2
-        dp=set()
-        dp.add(0) # base case
-        for i in range(len(nums)-1,-1,-1):
-            next_dp=set()
-            for x in dp:
-                if x+nums[i]==target:
-                    return True
-                next_dp.add(x)
-                next_dp.add(x+nums[i])
-            dp=next_dp
-        return False
+        memo={}
+        def dfs(i,cur_sum):
+            if (i,cur_sum) in memo:
+                return memo[i,cur_sum]
+            if i>=len(nums) or cur_sum>total-cur_sum:
+                return False
+            if cur_sum==total-cur_sum:
+                return True
+            memo[i,cur_sum]= dfs(i+1,cur_sum) or dfs(i+1,cur_sum+nums[i])
+            return memo[i,cur_sum]
+        return dfs(0,0)
