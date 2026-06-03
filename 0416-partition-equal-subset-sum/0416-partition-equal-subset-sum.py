c7b7c06a1b2483ffe01d8ec080c838ed0e54cfe4
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2026-06-03 21:08:52 +0530

    Time: 2275 ms (11.71%), Space: 34.9 MB (34.03%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,17 +1,24 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
+        # when convert to memo -> dp follow these steps
+        # identify the states dfs(i,cur)-> dp[i][cur]
+        n=len(nums)
         total=sum(nums)
+        target=total//2
+        dp=[[False]*(target+1) for _ in range(n+1)]
         if total%2!=0:
             return False
-        
-        cache={}
-        def dfs(i,cur):
-            if (i,cur) in cache:
-                return cache[i,cur]
-            if i>=len(nums) or cur>total-cur:
-                return False
-            if cur==total-cur:
-                return True
-            cache[i,cur] = dfs(i+1,cur) or dfs(i+1,cur+nums[i]) 
-            return cache[i,cur]
-        return dfs(0,0)
+        # identify the base cases
+        # i>=n or cur>total-cur -> False
+        # cur==total-cur -> True
+        for i in range(n+1):
+            dp[i][target]=True
+        for i in range(n-1,-1,-1):
+            for cur in range(target):
+                if cur>total-cur:
+                    dp[i][cur]=False
+                else:
+                    skip=dp[i+1][cur]
+                    include=dp[i+1][cur+nums[i]] if cur+nums[i]<=target else False
+                    dp[i][cur]=skip or include
+        return dp[0][0]
