a161312d40708b27cafd01c9094ed6d929f491ee
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2026-06-03 20:35:05 +0530

    Time: 765 ms (30.33%), Space: 50.7 MB (37.06%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.py
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.py
@@ -8,13 +8,19 @@ class Solution:
         dp=[0]*(n+1)
         # identify the base case if i>=len(s): return 0
         # already we set as 0
+        # precompute the palindrome
+        is_palindrome=[[False]*(n) for _ in range(n)]
+        for i in range(n-1,-1,-1): # once again want to revisit this palindrome portion
+            for j in range(i,n):
+                if s[i]==s[j] and (j-i<=2 or is_palindrome[i+1][j-1]):
+                    is_palindrome[i][j]=True
         # iterate from right to left
         for i in range(n-1,-1,-1):
             temp=""
             res=inf
             for j in range(i,n):
                 temp+=s[j]
-                if temp==temp[::-1]:
+                if is_palindrome[i][j]:
                     cost=1+dp[j+1]
                     res=min(res,cost)
                 dp[i]=res
