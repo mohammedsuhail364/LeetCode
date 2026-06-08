2a56c5ad86803cbbd6c05f9b965a56111f876c09
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2026-06-08 21:18:16 +0530

    Time: 523 ms (54.16%), Space: 19.8 MB (38.72%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,18 +1,24 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        cache={}
-        def dfs(cur):
-            if cur in cache:
-                return cache[cur]
-            if cur>amount:
-                return inf
-            if cur==amount:
-                return 0
-            cache[cur]=inf
+        dp=[inf]*(amount+1)
+        dp[amount]=0
+        for cur in range(amount,-1,-1):
             for c in coins:
-                cache[cur]=min(cache[cur],1+dfs(cur+c))
-            return cache[cur]
-        res = dfs(0)
-        if res==inf:
-            return -1
-        return res
+                dp[cur]=min(dp[cur],1+(dp[cur+c] if cur+c<=amount else inf))
+        return -1 if dp[0]==inf else dp[0]
+        # cache={}
+        # def dfs(cur):
+        #     if cur in cache:
+        #         return cache[cur]
+        #     if cur>amount:
+        #         return inf
+        #     if cur==amount:
+        #         return 0
+        #     cache[cur]=inf
+        #     for c in coins:
+        #         cache[cur]=min(cache[cur],1+dfs(cur+c))
+        #     return cache[cur]
+        # res = dfs(0)
+        # if res==inf:
+        #     return -1
+        # return res
