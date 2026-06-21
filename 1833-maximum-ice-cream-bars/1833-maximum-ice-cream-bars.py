1edab6f9a0c5e7b72853b19fd9558906845b8301
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2026-06-21 07:57:58 +0530

    Time: 88 ms (36.59%), Space: 31.1 MB (76.9%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.py
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.py
@@ -1,10 +1,19 @@
+from collections import defaultdict
+from typing import List
 class Solution:
     def maxIceCream(self, costs: List[int], coins: int) -> int:
-        costs.sort()
-        res=0
+        n=max(costs)
+        counter=[0]*(n+1)
         for c in costs:
-            if coins<c:
+            counter[c]+=1
+        res=0
+        for cost in range(len(counter)):
+            if counter[cost]==0:
+                continue
+            if cost>coins:
                 break
-            coins-=c
-            res+=1
+            count=min(counter[cost],coins//cost) # coins//cost -> we can easily find how much we can afford
+            coins-=(cost*count)
+            res+=count
+
         return res
