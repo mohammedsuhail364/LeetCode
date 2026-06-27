573039575766c9ebe083e8da6fdaa05ff2b050e2
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2026-06-27 09:22:07 +0530

    Time: 19 ms (60.69%), Space: 19.5 MB (18.07%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,20 +1,19 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        def convert_to_num(i):
-            return ord(i)-ord('a')
-        s1_count=[0]*26
-        s2_count=[0]*26
+        _s1 = [0]*26
+        _s2 = [0]*26
         for i in s1:
-            idx=convert_to_num(i)
-            s1_count[idx]+=1
+            idx=ord(i)-ord('a')
+            _s1[idx]+=1
         l=0
         for r in range(len(s2)):
-            idx=convert_to_num(s2[r])
-            s2_count[idx]+=1
+            
             if (r-l+1)>len(s1):
-                idx=convert_to_num(s2[l])
-                s2_count[idx]-=1
+                idx=ord(s2[l])-ord('a')
                 l+=1
-            if (r-l+1)==len(s1) and s1_count==s2_count :
+                _s2[idx]-=1
+            idx=ord(s2[r])-ord('a')
+            _s2[idx]+=1
+            if (r-l+1)==len(s1) and _s1==_s2:
                 return True
         return False
