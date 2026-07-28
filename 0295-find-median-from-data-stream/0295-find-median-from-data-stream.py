fed21e3e5d40675a2620f73e2f1259b4d77c824d
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2026-07-28 20:33:58 +0530

    Time: 209 ms (32.03%), Space: 41.8 MB (74.66%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -1,20 +1,22 @@
 class MedianFinder:
 
     def __init__(self):
-        self.nums=[]
+        # this question basically use the two heap pattern 
+        # one heap contains the lower half with negatives 
+        # another heap contains the upper half with positives
+        self.maxHeap=[]  #lower half with negatives 
+        self.minHeap=[] # upper half with positives
 
     def addNum(self, num: int) -> None:
-        self.nums.append(num)
+        heappush(self.maxHeap,-num)
+        heappush(self.minHeap,-heappop(self.maxHeap))
+        if len(self.minHeap)>len(self.maxHeap):
+            heappush(self.maxHeap,-heappop(self.minHeap))
 
     def findMedian(self) -> float:
-        self.nums.sort()
-        mid=len(self.nums)//2
-        if len(self.nums)%2:
-            return self.nums[mid]
-        else:
-            val=(self.nums[mid]+self.nums[mid-1])/2
-            return val
-
+        if len(self.maxHeap)>len(self.minHeap):
+            return -self.maxHeap[0]
+        return (-self.maxHeap[0]+self.minHeap[0])/2
 
 
 # Your MedianFinder object will be instantiated and called as such:
