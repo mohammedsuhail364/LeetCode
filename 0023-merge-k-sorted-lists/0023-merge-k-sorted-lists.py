37562a7f9035bb0c20ef1ffb0525c365f631116a
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2026-08-05 20:42:07 +0530

    Time: 11 ms (52.86%), Space: 23.1 MB (28.16%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -7,28 +7,16 @@ class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         if not lists or len(lists)==0:
             return None
-        while len(lists)>1:
-            res=[]
-            
-            for i in range(0,len(lists),2):
-                a=lists[i]
-                b=lists[i+1] if i+1<len(lists) else None
-                res.append(self.mergeTwoNodes(a,b))
-            lists = res
-        return lists[0]
-    def mergeTwoNodes(self,a,b):
-        node=ListNode()
-        res=node
-        while a and b:
-            if a.val>b.val:
-                node.next=b
-                b=b.next
-            else:
-                node.next=a
-                a=a.next
-            node=node.next
-        if a:
-            node.next=a
-        else:
-            node.next=b
-        return res.next
+        min_heap=[] # min heap
+        for i,l in enumerate(lists):
+            if l:
+                heappush(min_heap,(l.val,i,l))
+        dummy=ListNode(0)
+        cur=dummy
+        while min_heap:
+            val,i,node = heappop(min_heap)
+            cur.next=node
+            cur=cur.next
+            if node.next:
+                heappush(min_heap,(node.next.val,i,node.next))
+        return dummy.next
