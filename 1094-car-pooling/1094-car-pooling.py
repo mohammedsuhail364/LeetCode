30c6071f109cdb9c594ea6ecf9e0b4a019bdda6b
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2026-08-23 08:37:17 +0530

    Time: 1 ms (73.2%), Space: 19.6 MB (54.24%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.py
+++ 1094-car-pooling/1094-car-pooling.py
@@ -1,19 +1,16 @@
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-        # 2 2 2 2 2
-        #     3 3 3 3 3
-        diff=[0]*1002
-        for inc,s,e in trips:
-            diff[s]+=inc
-            if e<len(diff):
-                diff[e]-=inc
-        print(diff)
-        cur_seat=0
-        for n in diff:
-            cur_seat+=n
-            if cur_seat>capacity:
+        trips.sort(key=lambda x:x[1])
+        minHeap=[]
+        cur=0
+        for pas,s,e in trips:
+            while minHeap and minHeap[0][0]<=s:
+                end,p = heappop(minHeap)
+                cur-=p
+            cur+=pas
+            heappush(minHeap,(e,pas))
+            if cur>capacity:
                 return False
         return True
-        # [0,0,0,0,0,0,0] 
-        # +2         -2 
-        #                    
+        
+                    
