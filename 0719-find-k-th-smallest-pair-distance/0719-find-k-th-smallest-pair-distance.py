eaf5989c82c596c02db4c5e6153242a8e3b4b804
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2026-09-07 21:16:53 +0530

    Time: 31 ms (67.17%), Space: 19.8 MB (94.08%) - LeetHub

--- 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.py
+++ 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.py
@@ -1,24 +1,21 @@
 class Solution:
     def smallestDistancePair(self, nums: List[int], k: int) -> int:
-        def findPairsBelowMid(max_dist):
+        def findPairsBelowM(m):
             l=0
-            res=0
+            pairs=0
             for r in range(len(nums)):
-                while nums[r]-nums[l]>max_dist:
+                while nums[r]-nums[l]>m:
                     l+=1
-                res+=(r-l) # add only valid pairs
-            return res
-        nums.sort() # important one
-        # in this we use the binary search we can take the number and check there is atleast k pairs 
+                pairs+=(r-l)
+            return pairs
+        # refer neetcode
+        nums.sort()
         l=0
         r=nums[-1]-nums[0]
-
         while l<r:
-            m=(l+r)//2
-            pairs=findPairsBelowMid(m)
-            if pairs>=k:
-
+            m=l+((r-l)//2)
+            if findPairsBelowM(m)>=k:
                 r=m
             else:
                 l=m+1
-        return l
+        return r
