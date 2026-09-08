d08708e68db2b7714eda0f83d7e0053a77eece67
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2026-09-08 20:35:38 +0530

    Time: 47 ms (41.79%), Space: 19.4 MB (57.75%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.py
+++ 0264-ugly-number-ii/0264-ugly-number-ii.py
@@ -1,14 +1,16 @@
 class Solution:
     def nthUglyNumber(self, n: int) -> int:
         # refer neetcode
-        minHeap=[1]
-        factors=[2,3,5]
-        visit=set([1])
-        for i in range(n):
-            num=heappop(minHeap)
-            if i==n-1:
-                return num
-            for f in factors:
-                if num*f not in visit:
-                    visit.add(num*f)
-                    heappush(minHeap,num*f)
+        nums=[1]
+        i2,i3,i5=0,0,0
+        for i in range(1,n):
+            next_num=min(nums[i2]*2,nums[i3]*3,nums[i5]*5)
+            if next_num==nums[i2]*2:
+                i2+=1
+            if next_num==nums[i3]*3:
+                i3+=1
+            if next_num==nums[i5]*5:
+                i5+=1
+            nums.append(next_num)
+        return nums[n-1]
+                
